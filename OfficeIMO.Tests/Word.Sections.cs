037cea62e64c0dcb2c49342a2b771a43ba045010
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2022-01-01 11:30:01 +0100

    Commited tests that should fail

---
+++ OfficeIMO.Tests/Word.Sections.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace OfficeIMO.Tests {
+    public partial class Word {
+        [Fact]
+        public void Test_OpeningWordWithSections() {
+            using (WordDocument document = WordDocument.Load(Path.Combine(_directoryWithFiles, "BasicDocumentWithSections.docx"))) {
+                // There is only one Paragraph at the document level.
+                Assert.True(document.Paragraphs.Count == 3);
+
+                // There is only one PageBreak in this document.
+                Assert.True(document.Sections.Count == 3);
+
+                // This table has 12 Paragraphs.
+                //Assert.True(t0.Paragraphs.Count() == 12);
+            }
+        }
+    }
+}
