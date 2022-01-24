5bab72de687e966b816f2702154531d8935b936a
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2022-01-24 14:22:18 +0100

    New class of helpers, cleaning up

---
+++ OfficeIMO/WordHelpers.Colors.cs
@@ -0,0 +1,19 @@
+﻿namespace OfficeIMO {
+    public static partial class WordHelpers {
+
+        /// <summary>
+        /// Converts Color to Hex Color
+        /// </summary>
+        /// <param name="c"></param>
+        /// <returns></returns>
+        public static string ToHexColor(this System.Drawing.Color c) {
+            return "#" + c.R.ToString("X2") + c.G.ToString("X2") + c.B.ToString("X2");
+        }
+        /// <summary>
+        /// Converts Color to RGB Color
+        /// </summary>
+        /// <param name="c"></param>
+        /// <returns></returns>
+        public static string ToRgbColor(this System.Drawing.Color c) => $"RGB({c.R}, {c.G}, {c.B})";
+    }
+}
