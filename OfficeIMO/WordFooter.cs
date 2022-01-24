aafcc8d13ac562a28661948f1d20bfdc75bb259c
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2022-01-24 14:21:54 +0100

    Moving methods a bit

--- OfficeIMO/WordFooter.cs
+++ OfficeIMO/WordFooter.cs
@@ -85,5 +85,22 @@ namespace OfficeIMO {
             var pageNumber = new WordPageNumber(_document, this, wordPageNumberStyle);
             return pageNumber;
         }
+
+        public static void RemoveFooters(WordprocessingDocument wordprocessingDocument) {
+            var docPart = wordprocessingDocument.MainDocumentPart;
+            DocumentFormat.OpenXml.Wordprocessing.Document document = docPart.Document;
+            if (docPart.FooterParts.Count() > 0) {
+                // Remove the header
+                docPart.DeleteParts(docPart.FooterParts);
+
+                // First, create a list of all descendants of type
+                // HeaderReference. Then, navigate the list and call
+                // Remove on each item to delete the reference.
+                var footers = document.Descendants<FooterReference>().ToList();
+                foreach (var footer in footers) {
+                    footer.Remove();
+                }
+            }
+        }
     }
 }
