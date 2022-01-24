ecc8c6d5d9ff8058a7cd3137d31a785e999d8eae
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2022-01-24 14:22:00 +0100

    Moving method

--- OfficeIMO/WordHeader.cs
+++ OfficeIMO/WordHeader.cs
@@ -80,5 +80,21 @@ namespace OfficeIMO {
             var pageNumber = new WordPageNumber(_document, this, wordPageNumberStyle);
             return pageNumber;
         }
+        public static void RemoveHeaders(WordprocessingDocument wordprocessingDocument) {
+            var docPart = wordprocessingDocument.MainDocumentPart;
+            DocumentFormat.OpenXml.Wordprocessing.Document document = docPart.Document;
+            if (docPart.HeaderParts.Count() > 0) {
+                // Remove the header
+                docPart.DeleteParts(docPart.HeaderParts);
+
+                // First, create a list of all descendants of type
+                // HeaderReference. Then, navigate the list and call
+                // Remove on each item to delete the reference.
+                var headers = document.Descendants<HeaderReference>().ToList();
+                foreach (var header in headers) {
+                    header.Remove();
+                }
+            }
+        }
     }
 }
