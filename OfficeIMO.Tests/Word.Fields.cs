e274fa31dfbcdc2e68b396073fe6e834d4fa706b
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2022-02-06 19:51:44 +0100

    Tests placeholders

---
+++ OfficeIMO.Tests/Word.Fields.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OfficeIMO.Tests {
+    public partial class Word {
+    }
+}
