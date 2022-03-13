5552b0f1afce80160b8377d2ca50b60f13b3a71e
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2022-03-13 09:50:28 +0100

    More tests for headers and footers

--- OfficeIMO.Tests/Word.HeadersAndFootersWithSections.cs
+++ OfficeIMO.Tests/Word.HeadersAndFootersWithSections.cs
@@ -1,4 +1,5 @@
-﻿using System.IO;
+﻿using System;
+using System.IO;
 using DocumentFormat.OpenXml.Wordprocessing;
 using OfficeIMO.Word;
 using Xunit;
@@ -14,7 +15,7 @@ namespace OfficeIMO.Tests {
                 document.AddParagraph("Test Section0");
                 document.AddHeadersAndFooters();
                 document.Sections[0].Header.Default.AddParagraph().SetText("Test Section 0 - Header");
-                
+
                 var section1 = document.AddSection();
                 section1.PageOrientation = PageOrientationValues.Portrait;
                 section1.AddParagraph("Test Section1");
@@ -51,7 +52,7 @@ namespace OfficeIMO.Tests {
                 Assert.True(document.Sections[0].Paragraphs.Count == 1, "Number of paragraphs on 1st section is wrong (load). Current: " + document.Sections[0].Paragraphs.Count);
             }
         }
-        
+
         [Fact]
         public void Test_CreatingWordDocumentWithSectionHeadersAndFootersAdvanced() {
             string filePath = Path.Combine(_directoryWithFiles, "CreatedDocumentWithHeadersAndFootersSection1.docx");
@@ -66,7 +67,7 @@ namespace OfficeIMO.Tests {
                 document.Sections[0].Header.First.AddParagraph().SetText("Test Section 0 - First Header");
                 Assert.True(document.Header.First.Paragraphs[0].Text == "Test Section 0 - First Header", "First Header Should be correct");
                 document.Sections[0].Header.Default.AddParagraph().SetText("Test Section 0 - Header");
-                
+
                 var section1 = document.AddSection();
                 section1.PageOrientation = PageOrientationValues.Portrait;
                 section1.AddParagraph("Test Section1");
@@ -233,7 +234,7 @@ namespace OfficeIMO.Tests {
                 Assert.True(document.Sections[1].Footer.First == null);
                 Assert.True(document.Sections[1].Header.Even == null);
                 Assert.True(document.Sections[1].Footer.Even == null);
-                
+
                 document.AddSection();
                 document.Sections[2].PageOrientation = PageOrientationValues.Landscape;
 
@@ -308,6 +309,110 @@ namespace OfficeIMO.Tests {
 
 
                 Assert.True(document.Sections.Count == 4, "Number of sections during creation is wrong.");
+
+                document.Save();
+            }
+        }
+
+
+        [Fact]
+        public void Test_CreatingWordDocumentHeadersAndFootersOddEvenFirst() {
+            string filePath = Path.Combine(_directoryWithFiles, "CreatedDocumentWithHeadersAndSectionsOddEventFirst.docx");
+            using (WordDocument document = WordDocument.Create(filePath)) {
+                var paragraph = document.AddParagraph("Basic paragraph");
+
+                document.AddHeadersAndFooters();
+                document.Sections[0].Header.Default.AddParagraph().AddText("Section 0");
+
+                var section1 = document.AddSection();
+                section1.AddParagraph("Test Middle1 Section - 1");
+                section1.AddHeadersAndFooters();
+                section1.Header.Default.AddParagraph().AddText("Section 1 - Header");
+                section1.Footer.Default.AddParagraph().AddText("Section 1 - Footer");
+
+                var section2 = document.AddSection();
+                section2.AddParagraph("Test Middle2 Section - 1");
+                section2.AddHeadersAndFooters();
+                section2.Header.Default.AddParagraph().AddText("Section 2 - Header");
+                section2.Footer.Default.AddParagraph().AddText("Section 2 - Footer");
+
+                var section3 = document.AddSection();
+                section3.AddParagraph("Test Last Section - 1");
+                section3.AddHeadersAndFooters();
+                section3.DifferentOddAndEvenPages = true;
+                section3.DifferentFirstPage = true;
+                section3.Header.Default.AddParagraph().AddText("Section 3 - Header Odd/Default");
+                section3.Footer.Default.AddParagraph().AddText("Section 3 - Footer Odd/Default");
+                section3.Header.Even.AddParagraph().AddText("Section 3 - Header Even");
+                section3.Footer.Even.AddParagraph().AddText("Section 3 - Footer Even");
+
+                document.AddPageBreak();
+                section3.AddParagraph("Test Last Section - 2");
+                document.AddPageBreak();
+                section3.AddParagraph("Test Last Section - 3");
+
+                document.Save(false);
+
+                Assert.True(document.Sections[0].DifferentOddAndEvenPages == false);
+                Assert.True(document.Sections[0].DifferentFirstPage == false);
+
+                Assert.True(document.Sections[1].DifferentOddAndEvenPages == false);
+                Assert.True(document.Sections[1].DifferentFirstPage == false);
+
+                Assert.True(document.Sections[2].DifferentOddAndEvenPages == false);
+                Assert.True(document.Sections[2].DifferentFirstPage == false);
+
+                Assert.True(document.Sections[3].DifferentOddAndEvenPages == true);
+                Assert.True(document.Sections[3].DifferentFirstPage == true);
+
+                Assert.True(document.Sections[3].Paragraphs[0].Text == "Test Last Section - 1");
+
+            }
+            using (WordDocument document = WordDocument.Load(Path.Combine(_directoryWithFiles, "CreatedDocumentWithHeadersAndSectionsOddEventFirst.docx"))) {
+                Assert.True(document.Sections[0].DifferentOddAndEvenPages == false);
+                Assert.True(document.Sections[0].DifferentFirstPage == false);
+
+                Assert.True(document.Sections[1].DifferentOddAndEvenPages == false);
+                Assert.True(document.Sections[1].DifferentFirstPage == false);
+
+                Assert.True(document.Sections[2].DifferentOddAndEvenPages == false);
+                Assert.True(document.Sections[2].DifferentFirstPage == false);
+
+                Assert.True(document.Sections[3].DifferentOddAndEvenPages == true);
+                Assert.True(document.Sections[3].DifferentFirstPage == true);
+
+                document.Sections[1].DifferentOddAndEvenPages = true;
+                document.Sections[2].DifferentFirstPage = true;
+
+                Assert.True(document.Sections[0].DifferentOddAndEvenPages == false);
+                Assert.True(document.Sections[0].DifferentFirstPage == false);
+
+                Assert.True(document.Sections[1].DifferentOddAndEvenPages == true);
+                Assert.True(document.Sections[1].DifferentFirstPage == false);
+
+                Assert.True(document.Sections[2].DifferentOddAndEvenPages == false);
+                Assert.True(document.Sections[2].DifferentFirstPage == true);
+
+                Assert.True(document.Sections[3].DifferentOddAndEvenPages == true);
+                Assert.True(document.Sections[3].DifferentFirstPage == true);
+                Assert.True(document.Sections[3].Paragraphs[0].Text == "Test Last Section - 1");
+                document.Save();
+            }
+
+            using (WordDocument document = WordDocument.Load(Path.Combine(_directoryWithFiles, "CreatedDocumentWithHeadersAndSectionsOddEventFirst.docx"))) {
+                Assert.True(document.Sections[0].DifferentOddAndEvenPages == false);
+                Assert.True(document.Sections[0].DifferentFirstPage == false);
+
+                Assert.True(document.Sections[1].DifferentOddAndEvenPages == true);
+                Assert.True(document.Sections[1].DifferentFirstPage == false);
+
+                Assert.True(document.Sections[2].DifferentOddAndEvenPages == false);
+                Assert.True(document.Sections[2].DifferentFirstPage == true);
+
+                Assert.True(document.Sections[3].DifferentOddAndEvenPages == true);
+                Assert.True(document.Sections[3].DifferentFirstPage == true);
+
+                Assert.True(document.Sections[3].Paragraphs[0].Text == "Test Last Section - 1");
 
                 document.Save();
             }
