db9785c50994246bbb8e5bfddd83eb820b2ee0fb
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2022-07-10 21:08:44 +0200

    Cleanup comments

---
+++ OfficeIMO.Word/WordTable.Methods.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using DocumentFormat.OpenXml;
+using DocumentFormat.OpenXml.Wordprocessing;
+
+namespace OfficeIMO.Word {
+    public partial class WordTable {
+        /// <summary>
+        /// Add comment to a Table
+        /// </summary>
+        /// <param name="author"></param>
+        /// <param name="initials"></param>
+        /// <param name="comment"></param>
+        public void AddComment(string author, string initials, string comment) {
+            WordComment wordComment = WordComment.Create(_document, author, initials, comment);
+            InsertComment(wordComment,
+                this.FirstRow.FirstCell.Paragraphs[0]._paragraph,
+                this.LastRow.LastCell.Paragraphs[0]._paragraph,
+                this.LastRow.LastCell.Paragraphs[0]._paragraph);
+        }
+
+        internal void InsertComment(WordComment wordComment, OpenXmlElement rangeStart, OpenXmlElement rangeEnd, OpenXmlElement reference) {
+            // Specify the text range for the Comment.
+            // Insert the new CommentRangeStart before the first run of paragraph.
+            rangeStart.InsertBefore(new CommentRangeStart() { Id = wordComment.Id }, rangeStart.GetFirstChild<OpenXmlElement>());
+
+            // Insert the new CommentRangeEnd after last run of paragraph.
+            var cmtEnd = rangeEnd.InsertAfter(new CommentRangeEnd() { Id = wordComment.Id }, rangeEnd.Elements().Last());
+
+            // Compose a run with CommentReference and insert it.
+            reference.InsertAfter(new Run(new CommentReference() { Id = wordComment.Id }), cmtEnd);
+        }
+    }
+}
