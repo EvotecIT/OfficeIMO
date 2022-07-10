08c278a75b4616f032a44591482536fa33a39bc4
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2022-07-10 20:36:27 +0200

    Add Autofit WIP

---
+++ OfficeIMO.Word/WordTable.Properties.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OfficeIMO.Word {
+    public partial class WordTable {
+        public enum AutoFitType {
+            ToContent,
+            ToWindow,
+            Fixed
+        }
+
+        public AutoFitType? AutoFit {
+            get {
+                return AutoFitType.ToContent;
+            }
+        }
+    }
+}
