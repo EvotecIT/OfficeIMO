ce93338d686b1635d5183f7162f523c7b992b333
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2022-07-30 16:29:15 +0200

    Add new properties Table properties

--- OfficeIMO.Word/WordTable.Properties.cs
+++ OfficeIMO.Word/WordTable.Properties.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using DocumentFormat.OpenXml.Wordprocessing;
 
 namespace OfficeIMO.Word {
     public partial class WordTable {
@@ -15,5 +16,87 @@ namespace OfficeIMO.Word {
                 return AutoFitType.ToContent;
             }
         }
+
+        /// <summary>
+        /// Gets or sets a Title/Caption to a Table
+        /// </summary>
+        public string Title {
+            get {
+                if (_tableProperties != null && _tableProperties.TableCaption != null) {
+                    return _tableProperties.TableCaption.Val;
+                }
+
+                return null;
+            }
+            set {
+                CheckTableProperties();
+                if (_tableProperties.TableCaption == null) {
+                    _tableProperties.TableCaption = new TableCaption();
+                }
+                if (value != null) {
+                    _tableProperties.TableCaption.Val = value;
+                } else {
+                    _tableProperties.TableCaption.Remove();
+                }
+            }
+        }
+
+        /// <summary>
+        /// Gets or sets Description for a Table
+        /// </summary>
+        public string Description {
+            get {
+                if (_tableProperties != null && _tableProperties.TableDescription != null) {
+                    return _tableProperties.TableDescription.Val;
+                }
+
+                return null;
+            }
+            set {
+                CheckTableProperties();
+                if (_tableProperties.TableDescription == null) {
+                    _tableProperties.TableDescription = new TableDescription();
+                }
+                if (value != null) {
+                    _tableProperties.TableDescription.Val = value;
+                } else {
+                    _tableProperties.TableDescription.Remove();
+                }
+            }
+        }
+
+        /// <summary>
+        /// Allow table to overlap or not
+        /// </summary>
+        public bool AllowOverlap {
+            get {
+                if (this.Position.TableOverlap == TableOverlapValues.Overlap) {
+                    return true;
+                }
+                return false;
+            }
+            set => this.Position.TableOverlap = value ? TableOverlapValues.Overlap : TableOverlapValues.Never;
+        }
+
+        /// <summary>
+        /// Allow text to wrap around table.
+        /// </summary>
+        public bool AllowTextWrap {
+            get {
+                if (this.Position.VerticalAnchor == VerticalAnchorValues.Text) {
+                    return true;
+                }
+
+                return false;
+            }
+            set {
+                if (value == true) {
+                    this.Position.VerticalAnchor = VerticalAnchorValues.Text;
+                } else {
+                    this.Position.VerticalAnchor = null;
+                }
+            }
+        }
+
     }
 }
