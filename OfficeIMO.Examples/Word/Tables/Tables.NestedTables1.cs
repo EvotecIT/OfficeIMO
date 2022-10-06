1e6f510eea45bfc3368f674cab5d7080e099c7d0
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2022-10-06 18:52:03 +0200

    Update example

--- OfficeIMO.Examples/Word/Tables/Tables.NestedTables1.cs
+++ OfficeIMO.Examples/Word/Tables/Tables.NestedTables1.cs
@@ -1,11 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using DocumentFormat.OpenXml.Wordprocessing;
 using OfficeIMO.Word;
-using Color = SixLabors.ImageSharp.Color;
 
 namespace OfficeIMO.Examples.Word {
     internal static partial class Tables {
