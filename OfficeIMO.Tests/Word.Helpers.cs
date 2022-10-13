9c0fcce8c84fa0370e450e3745308f50897eca5f
Rustam Sayfutdinov <rstm.sf@gmail.com>
2022-10-13 23:01:00 +0300

    Add tests for GetImageСharacteristics

---
+++ OfficeIMO.Tests/Word.Helpers.cs
@@ -0,0 +1,21 @@
+﻿using System.IO;
+using DocumentFormat.OpenXml.Packaging;
+using OfficeIMO.Word;
+using Xunit;
+
+namespace OfficeIMO.Tests;
+
+public partial class Word {
+    [Theory]
+    [InlineData("snail.bmp", ImagePartType.Bmp)]
+    [InlineData("example.gif", ImagePartType.Gif)]
+    [InlineData("Kulek.jpg", ImagePartType.Jpeg)]
+    [InlineData("BackgroundImage.png", ImagePartType.Png)]
+    [InlineData("saturn.tif", ImagePartType.Tiff)]
+    public void Test_GetImageСharacteristics(string filename, ImagePartType expectedType) {
+        var filePath = Path.Combine(_directoryWithImages, filename);
+        using var imageStream = new FileStream(filePath, FileMode.Open);
+        var imageСharacteristics = Helpers.GetImageСharacteristics(imageStream);
+        Assert.Equal(expectedType, imageСharacteristics.Type);
+    }
+}
