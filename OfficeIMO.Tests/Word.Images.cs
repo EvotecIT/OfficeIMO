d7cdedf9542e3a5ffc0423e5e8a5de8b3136a636
Rustam Sayfutdinov <rstm.sf@gmail.com>
2022-10-17 08:38:25 +0300

    Refactor test Word.Images

--- OfficeIMO.Tests/Word.Images.cs
+++ OfficeIMO.Tests/Word.Images.cs
@@ -1,8 +1,8 @@
-using System;
 using System.IO;
 using DocumentFormat.OpenXml.Drawing;
 using OfficeIMO.Word;
 using Xunit;
+
 using Path = System.IO.Path;
 
 namespace OfficeIMO.Tests {
@@ -10,75 +10,74 @@ namespace OfficeIMO.Tests {
 
         [Fact]
         public void Test_CreatingWordDocumentWithImages() {
-            string filePath = Path.Combine(_directoryWithFiles, "CreatedDocumentWithImages.docx");
+            var filePath = Path.Combine(_directoryWithFiles, "CreatedDocumentWithImages.docx");
+            using var document = WordDocument.Create(filePath);
 
-            using (WordDocument document = WordDocument.Create(filePath)) {
-                document.BuiltinDocumentProperties.Title = "This is sparta";
-                document.BuiltinDocumentProperties.Creator = "Przemek";
+            document.BuiltinDocumentProperties.Title = "This is sparta";
+            document.BuiltinDocumentProperties.Creator = "Przemek";
 
-                var paragraph = document.AddParagraph("This paragraph starts with some text");
-                paragraph.Text = "0th This paragraph started with some other text and was overwritten and made bold.";
+            var paragraph = document.AddParagraph("This paragraph starts with some text");
+            paragraph.Text = "0th This paragraph started with some other text and was overwritten and made bold.";
 
-                // lets add image to paragraph
-                paragraph.AddImage(System.IO.Path.Combine(_directoryWithImages, "PrzemyslawKlysAndKulkozaurr.jpg"), 22, 22);
-                //paragraph.Image.WrapText = true; // WrapSideValues.Both;
+            // lets add image to paragraph
+            paragraph.AddImage(Path.Combine(_directoryWithImages, "PrzemyslawKlysAndKulkozaurr.jpg"), 22, 22);
+            //paragraph.Image.WrapText = true; // WrapSideValues.Both;
 
-                var paragraph5 = paragraph.AddText("and more text");
-                paragraph5.Bold = true;
+            var paragraph5 = paragraph.AddText("and more text");
+            paragraph5.Bold = true;
 
-                Assert.True(paragraph.Image.Width == 22);
-                Assert.True(paragraph.Image.Width == 22);
+            Assert.Equal(22d, paragraph.Image.Height!.Value, 15);
+            Assert.Equal(22d, paragraph.Image.Width!.Value, 15);
 
-                document.AddParagraph("This adds another picture with 500x500");
+            document.AddParagraph("This adds another picture with 500x500");
 
-                var filePathImage = System.IO.Path.Combine(_directoryWithImages, "Kulek.jpg");
-                WordParagraph paragraph2 = document.AddParagraph();
-                paragraph2.AddImage(filePathImage, 500, 500);
-                //paragraph2.Image.BlackWiteMode = BlackWhiteModeValues.GrayWhite;
-                paragraph2.Image.Rotation = 180;
-                paragraph2.Image.Shape = ShapeTypeValues.ActionButtonMovie;
+            var filePathImage = Path.Combine(_directoryWithImages, "Kulek.jpg");
+            var paragraph2 = document.AddParagraph();
+            paragraph2.AddImage(filePathImage, 500, 500);
+            //paragraph2.Image.BlackWiteMode = BlackWhiteModeValues.GrayWhite;
+            paragraph2.Image.Rotation = 180;
+            paragraph2.Image.Shape = ShapeTypeValues.ActionButtonMovie;
 
-                Assert.True(paragraph2.Image.Height == 500);
-                Assert.True(paragraph2.Image.Width == 500);
+            Assert.Equal(500d, paragraph2.Image.Height!.Value, 15);
+            Assert.Equal(500d, paragraph2.Image.Width!.Value, 15);
 
-                document.AddParagraph("This adds another picture with 100x100");
+            document.AddParagraph("This adds another picture with 100x100");
 
-                WordParagraph paragraph3 = document.AddParagraph();
-                paragraph3.AddImage(filePathImage, 100, 100);
+            var paragraph3 = document.AddParagraph();
+            paragraph3.AddImage(filePathImage, 100, 100);
 
-                // we add paragraph with an image
-                WordParagraph paragraph4 = document.AddParagraph();
-                paragraph4.AddImage(filePathImage);
+            // we add paragraph with an image
+            var paragraph4 = document.AddParagraph();
+            paragraph4.AddImage(filePathImage);
 
-                // we can also overwrite height later on
-                paragraph4.Image.Height = 50;
-                paragraph4.Image.Width = 50;
-                // this doesn't work
-                paragraph4.Image.HorizontalFlip = true;
+            // we can also overwrite height later on
+            paragraph4.Image.Height = 50;
+            paragraph4.Image.Width = 50;
+            // this doesn't work
+            paragraph4.Image.HorizontalFlip = true;
 
 
-                Assert.True(paragraph4.Image.Height == 50);
-                Assert.True(paragraph4.Image.Width == 50);
+            Assert.Equal(50d, paragraph4.Image.Height.Value, 15);
+            Assert.Equal(50d, paragraph4.Image.Width.Value, 15);
 
-                // or we can get any image and overwrite it's size
-                document.Images[0].Height = 200;
-                document.Images[0].Width = 200;
+            // or we can get any image and overwrite it's size
+            document.Images[0].Height = 200;
+            document.Images[0].Width = 200;
 
-                Assert.True(document.Images[0].Height == 200);
-                Assert.True(document.Images[0].Height == 200);
+            Assert.Equal(200d, document.Images[0].Height.Value, 15);
+            Assert.Equal(200d, document.Images[0].Width.Value, 15);
 
-                string fileToSave = System.IO.Path.Combine(_directoryDocuments, "CreatedDocumentWithImagesPrzemyslawKlysAndKulkozaurr.jpg");
-                document.Images[0].SaveToFile(fileToSave);
+            var fileToSave = Path.Combine(_directoryDocuments, "CreatedDocumentWithImagesPrzemyslawKlysAndKulkozaurr.jpg");
+            document.Images[0].SaveToFile(fileToSave);
 
-                var fileInfo = new FileInfo(fileToSave);
+            var fileInfo = new FileInfo(fileToSave);
 
-                Assert.True(fileInfo.Length > 0);
-                Assert.True(File.Exists(fileToSave) == true);
+            Assert.True(fileInfo.Length > 0);
+            Assert.True(File.Exists(fileToSave));
 
-                Assert.True(document.Images.Count == 4);
-                Assert.True(document.Sections[0].Images.Count == 4);
-                document.Save(false);
-            }
+            Assert.Equal(4, document.Images.Count);
+            Assert.Equal(4, document.Sections[0].Images.Count);
+            document.Save(false);
         }
     }
 }
