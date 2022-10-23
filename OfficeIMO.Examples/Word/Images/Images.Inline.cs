a5f84bb1b864e980d69726051a96826b83a4f38d
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2022-10-23 22:04:30 +0200

    Improve example

--- OfficeIMO.Examples/Word/Images/Images.Inline.cs
+++ OfficeIMO.Examples/Word/Images/Images.Inline.cs
@@ -16,16 +16,18 @@ namespace OfficeIMO.Examples.Word {
             using (WordDocument document = WordDocument.Create(filePath)) {
                 var file = System.IO.Path.Combine(imagePaths, "PrzemyslawKlysAndKulkozaurr.jpg");
                 var paragraph = document.AddParagraph();
-                var pargraphWithImage = paragraph.AddImage(file, 100, 100);
+                var pargraphWithImage = paragraph.AddImage(file, 100, 100, "Przemek and Kulek on an image");
 
-                // Console.WriteLine("Image is inline: " + pargraphWithImage.Image.Rotation);
-
+                Console.WriteLine("Image is inline: " + pargraphWithImage.Image.Rotation);
 
                 pargraphWithImage.Image.VerticalFlip = false;
                 pargraphWithImage.Image.HorizontalFlip = false;
-                pargraphWithImage.Image.Rotation = 270;
+                pargraphWithImage.Image.Rotation = 190;
                 pargraphWithImage.Image.Shape = ShapeTypeValues.Cloud;
+                pargraphWithImage.Image.BlackWiteMode = BlackWhiteModeValues.GrayWhite;
+                pargraphWithImage.Image.Description = "Other description";
 
+                Console.WriteLine("Image is inline: " + pargraphWithImage.Image.Rotation);
 
                 document.Save(openWord);
             }
