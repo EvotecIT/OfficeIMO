fd41afa42d36e02409815c1bb0dbd0d85c683996
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2022-11-06 19:32:20 +0100

    Small improvement to examples

--- OfficeIMO.Examples/Program.cs
+++ OfficeIMO.Examples/Program.cs
@@ -1,11 +1,8 @@
-using DocumentFormat.OpenXml.Drawing;
 using DocumentFormat.OpenXml.Wordprocessing;
 using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
-using DocumentFormat.OpenXml.Packaging;
-using OfficeIMO.Examples.Excel;
 using OfficeIMO.Examples.Word;
 using OfficeIMO.Word;
 using Color = SixLabors.ImageSharp.Color;
