76daa22f066348f929a433ed85cd535049ef3e51
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2023-01-30 16:34:14 +0100

    Improve example

--- OfficeIMO.Examples/Word/Embed/EmbedFileHTML.cs
+++ OfficeIMO.Examples/Word/Embed/EmbedFileHTML.cs
@@ -16,9 +16,23 @@ namespace OfficeIMO.Examples.Word {
             string filePath = System.IO.Path.Combine(folderPath, "EmbeddedFileHTML.docx");
             string htmlFilePath = System.IO.Path.Combine(templateFolder, "SampleFileHTML.html");
             using (WordDocument document = WordDocument.Create(filePath)) {
+                Console.WriteLine("Embedded documents in word: " + document.EmbeddedDocuments.Count);
+                Console.WriteLine("Embedded documents in Section 0: " + document.Sections[0].EmbeddedDocuments.Count);
+
                 document.AddParagraph("Add HTML document in DOCX");
 
+                document.AddSection();
+
+                Console.WriteLine("Embedded documents in Section 1: " + document.Sections[1].EmbeddedDocuments.Count);
+
                 document.AddEmbeddedDocument(htmlFilePath);
+
+                document.EmbeddedDocuments[0].Save("C:\\TEMP\\EmbeddedFileHTML.html");
+
+                Console.WriteLine("Embedded documents in word: " + document.EmbeddedDocuments.Count);
+                Console.WriteLine("Embedded documents in Section 0: " + document.Sections[0].EmbeddedDocuments.Count);
+                Console.WriteLine("Embedded documents in Section 1: " + document.Sections[1].EmbeddedDocuments.Count);
+                Console.WriteLine("Content type: " + document.EmbeddedDocuments[0].ContentType);
 
                 document.Save(openWord);
             }
