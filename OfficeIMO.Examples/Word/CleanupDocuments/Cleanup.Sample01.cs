4c0c4187fe2f4aa64aef34a905921265d1a6b1a3
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2023-02-13 10:44:33 +0100

    Fix example

--- OfficeIMO.Examples/Word/CleanupDocuments/Cleanup.Sample01.cs
+++ OfficeIMO.Examples/Word/CleanupDocuments/Cleanup.Sample01.cs
@@ -5,26 +5,24 @@ using System.Text;
 using System.Threading.Tasks;
 using OfficeIMO.Word;
 
-namespace OfficeIMO.Examples.Word {
-    internal static partial class CleanupDocuments {
-        public static void CleanupDocuments_Sample01(bool openWord) {
-            Console.WriteLine("[*] Load external Word Document - Sample 1");
-            string documentPaths = System.IO.Path.Combine(System.IO.Directory.GetCurrentDirectory(), "Templates");
-            string fullPath = System.IO.Path.Combine(documentPaths, "sample1.docx");
-            using (WordDocument document = WordDocument.Load(fullPath, false)) {
-                Console.WriteLine(fullPath);
-                Console.WriteLine("Sections count: " + document.Sections.Count);
-                Console.WriteLine("Tables count: " + document.Tables.Count);
-                Console.WriteLine("Paragraphs count: " + document.Paragraphs.Count);
+internal static partial class CleanupDocuments {
+    public static void CleanupDocuments_Sample01(bool openWord) {
+        Console.WriteLine("[*] Load external Word Document - Sample 1");
+        string documentPaths = System.IO.Path.Combine(System.IO.Directory.GetCurrentDirectory(), "Templates");
+        string fullPath = System.IO.Path.Combine(documentPaths, "sample1.docx");
+        using (WordDocument document = WordDocument.Load(fullPath, false)) {
+            Console.WriteLine(fullPath);
+            Console.WriteLine("Sections count: " + document.Sections.Count);
+            Console.WriteLine("Tables count: " + document.Tables.Count);
+            Console.WriteLine("Paragraphs count: " + document.Paragraphs.Count);
 
-                var cleanupCount = document.CleanupDocument();
+            var cleanupCount = document.CleanupDocument();
 
-                Console.WriteLine("Removed " + cleanupCount + " runs because of identical formatting.");
+            Console.WriteLine("Removed " + cleanupCount + " runs because of identical formatting.");
 
-                Console.WriteLine("Paragraphs count: " + document.Paragraphs.Count);
+            Console.WriteLine("Paragraphs count: " + document.Paragraphs.Count);
 
-                document.Save(openWord);
-            }
+            document.Save(openWord);
         }
     }
 }
