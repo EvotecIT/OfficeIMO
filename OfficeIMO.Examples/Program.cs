338072f2fe4892d826ce4398896c0858a5ecd059
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2023-03-04 23:30:27 +0100

    Add example to the list

--- OfficeIMO.Examples/Program.cs
+++ OfficeIMO.Examples/Program.cs
@@ -10,9 +10,6 @@ namespace OfficeIMO.Examples {
         private static void Setup(string path) {
             if (!Directory.Exists(path)) {
                 Directory.CreateDirectory(path);
-            } else {
-                // Directory.Delete(path, false);
-                // Directory.CreateDirectory(path);
             }
         }
 
@@ -33,6 +30,7 @@ namespace OfficeIMO.Examples {
             Paragraphs.Example_BasicParagraphs(folderPath, false);
             Paragraphs.Example_BasicParagraphStyles(folderPath, false);
             Paragraphs.Example_MultipleParagraphsViaDifferentWays(folderPath, false);
+            Paragraphs.Example_BasicTabStops(folderPath, true);
 
             BasicDocument.Example_BasicDocument(folderPath, false);
             BasicDocument.Example_BasicDocumentSaveAs1(folderPath, false);
