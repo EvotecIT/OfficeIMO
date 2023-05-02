4d35858dc0152f2a76400b7965872c68185bcbc7
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2023-05-02 18:03:48 +0200

    Small test

--- OfficeIMO.Examples/Word/Charts/Charts.cs
+++ OfficeIMO.Examples/Word/Charts/Charts.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -18,41 +18,52 @@ namespace OfficeIMO.Examples.Word {
                     "Food", "Housing", "Mix", "Data"
                 };
 
-                document.AddParagraph("This is a bar chart");
-                var barChart1 = document.AddBarChart();
-                barChart1.AddCategories(categories);
-                barChart1.AddChartBar("Brazil", new List<int>() { 10, 35, 18, 23 }, SixLabors.ImageSharp.Color.Brown);
-                barChart1.AddChartBar("Poland", new List<int>() { 13, 20, 230, 150 }, SixLabors.ImageSharp.Color.Green);
-                barChart1.AddChartBar("USA", new[] { 10, 35, 18, 23 }, SixLabors.ImageSharp.Color.AliceBlue);
-                barChart1.BarGrouping = BarGroupingValues.Clustered;
-                barChart1.BarDirection = BarDirectionValues.Column;
+                //document.AddParagraph("This is a bar chart");
+                //var barChart1 = document.AddBarChart();
+                //barChart1.AddCategories(categories);
+                //barChart1.AddChartBar("Brazil", new List<int>() { 10, 35, 18, 23 }, SixLabors.ImageSharp.Color.Brown);
+                //barChart1.AddChartBar("Poland", new List<int>() { 13, 20, 230, 150 }, SixLabors.ImageSharp.Color.Green);
+                //barChart1.AddChartBar("USA", new[] { 10, 35, 18, 23 }, SixLabors.ImageSharp.Color.AliceBlue);
+                //barChart1.BarGrouping = BarGroupingValues.Clustered;
+                //barChart1.BarDirection = BarDirectionValues.Column;
 
-                document.AddParagraph("This is a bar chart");
-                var barChart2 = document.AddBarChart();
-                barChart2.AddCategories(categories);
-                barChart2.AddChartBar("USA", 15, Color.Aqua);
-                barChart2.RoundedCorners = true;
+                //document.AddParagraph("This is a bar chart");
+                //var barChart2 = document.AddBarChart();
+                //barChart2.AddCategories(categories);
+                //barChart2.AddChartBar("USA", 15, Color.Aqua);
+                //barChart2.RoundedCorners = true;
 
-                document.AddParagraph("This is a pie chart");
-                var pieChart = document.AddPieChart();
-                pieChart.AddCategories(categories);
-                pieChart.AddChartPie("Poland", new List<int> { 15, 20, 30 });
+                //document.AddParagraph("This is a pie chart");
+                //var pieChart = document.AddPieChart();
+                //pieChart.AddCategories(categories);
+                //pieChart.AddChartPie("Poland", new List<int> { 15, 20, 30 });
 
-                document.AddParagraph("Adding a line chart as required 1");
+                //document.AddParagraph("Adding a line chart as required 1");
 
-                var lineChart = document.AddLineChart();
-                lineChart.AddChartAxisX(categories);
-                lineChart.AddChartLine("USA", new List<int>() { 10, 35, 18, 23 }, SixLabors.ImageSharp.Color.AliceBlue);
-                lineChart.AddChartLine("Brazil", new List<int>() { 10, 35, 300, 18 }, SixLabors.ImageSharp.Color.Brown);
-                lineChart.AddChartLine("Poland", new List<int>() { 13, 20, 230, 150 }, SixLabors.ImageSharp.Color.Green);
+                //var lineChart = document.AddLineChart();
+                //lineChart.AddChartAxisX(categories);
+                //lineChart.AddChartLine("USA", new List<int>() { 10, 35, 18, 23 }, SixLabors.ImageSharp.Color.AliceBlue);
+                //lineChart.AddChartLine("Brazil", new List<int>() { 10, 35, 300, 18 }, SixLabors.ImageSharp.Color.Brown);
+                //lineChart.AddChartLine("Poland", new List<int>() { 13, 20, 230, 150 }, SixLabors.ImageSharp.Color.Green);
 
-                document.AddParagraph("Adding a line chart as required 2");
+                //document.AddParagraph("Adding a line chart as required 2");
 
-                var lineChart2 = document.AddLineChart();
-                lineChart2.AddChartAxisX(categories);
-                lineChart2.AddChartLine("USA", new List<int>() { 10, 35, 18, 23 }, SixLabors.ImageSharp.Color.AliceBlue);
-                lineChart2.AddChartLine("Brazil", new List<int>() { 10, 35, 300, 18 }, SixLabors.ImageSharp.Color.Brown);
-                lineChart2.AddChartLine("Poland", new List<int>() { 13, 20, 230, 150 }, SixLabors.ImageSharp.Color.Green);
+                //var lineChart2 = document.AddLineChart();
+                //lineChart2.AddChartAxisX(categories);
+                //lineChart2.AddChartLine("USA", new List<int>() { 10, 35, 18, 23 }, SixLabors.ImageSharp.Color.AliceBlue);
+                //lineChart2.AddChartLine("Brazil", new List<int>() { 10, 35, 300, 18 }, SixLabors.ImageSharp.Color.Brown);
+                //lineChart2.AddChartLine("Poland", new List<int>() { 13, 20, 230, 150 }, SixLabors.ImageSharp.Color.Green);
+
+
+                var paragraph = document.AddParagraph();
+
+                var barChart3 = paragraph.AddBarChart();
+                barChart3.AddChartBar("Brazil", new List<int>() { 10, 35, 18, 23 }, SixLabors.ImageSharp.Color.Brown);
+                barChart3.AddChartBar("Poland", new List<int>() { 13, 20, 230, 150 }, SixLabors.ImageSharp.Color.Green);
+                barChart3.AddChartBar("USA", new[] { 10, 35, 18, 23 }, SixLabors.ImageSharp.Color.AliceBlue);
+                barChart3.AddCategories(categories);
+                barChart3.BarGrouping = BarGroupingValues.Clustered;
+                barChart3.BarDirection = BarDirectionValues.Column;
 
                 document.Save(openWord);
             }
