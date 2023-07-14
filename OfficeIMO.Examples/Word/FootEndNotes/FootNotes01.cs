f6411e7d877644aab219a791ff1e73bc087b27aa
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2023-07-14 11:45:47 +0200

    Add basic example for endnotes/footnotes

---
+++ OfficeIMO.Examples/Word/FootEndNotes/FootNotes01.cs
@@ -0,0 +1,52 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using DocumentFormat.OpenXml.Drawing;
+using DocumentFormat.OpenXml.Wordprocessing;
+using OfficeIMO.Word;
+
+namespace OfficeIMO.Examples.Word {
+    internal static partial class FootNotes {
+        internal static void Example_DocumentWithFootNotes(string folderPath, bool openWord) {
+            Console.WriteLine("[*] Opening Document with foot notes");
+            var filePath = System.IO.Path.Combine(System.IO.Directory.GetCurrentDirectory(), "Templates", "DocumentWithFootNotes.docx");
+
+            using (WordDocument document = WordDocument.Load(filePath)) {
+                //foreach (var field in document.Fields) {
+                //    Console.WriteLine("...Type: " + field.FieldType);
+                //    Console.WriteLine("...Format switch: " + field.FieldFormat);
+                //    Console.WriteLine("...Instruction: " + String.Join(" ", field.FieldInstructions));
+                //    Console.WriteLine("...Switches: " + String.Join(" ", field.FieldSwitches));
+                //}
+
+                var fileTarget = System.IO.Path.Combine(System.IO.Directory.GetCurrentDirectory(), "Documents", "Document with FootNotes01.docx");
+                document.Save(fileTarget, openWord);
+            }
+        }
+
+        internal static void Example_DocumentWithFootNotesEmpty(string folderPath, bool openWord) {
+            Console.WriteLine("[*] Creating standard document with footnotes/end notes");
+            string filePath = System.IO.Path.Combine(folderPath, "Document with FootNotes02.docx");
+            using (WordDocument document = WordDocument.Create(filePath)) {
+                var paragraph = document.AddParagraph("Basic paragraph");
+                paragraph.ParagraphAlignment = JustificationValues.Center;
+
+                document.AddParagraph("This is my text").AddFootNote("This is a footnote to my text")
+                    .AddText(" continuing").AddFootNote("2nd footnote!");
+
+                document.AddParagraph("Another paragraph").AddFootNote("more footnotes!")
+                    .AddText(" more within paragraph").AddFootNote("4th footnote!");
+
+                document.AddParagraph("Testing endnote - 1").AddEndNote("Test end note 1");
+
+                document.AddParagraph("Test 1");
+
+                document.AddSection();
+
+                document.AddParagraph("Testing endnote - 2").AddEndNote("Test end note 2");
+
+                document.Save(openWord);
+            }
+        }
+    }
+}
