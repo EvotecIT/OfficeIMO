2250c68de9fdfe520c8066c1c059cffcf380921a
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2023-07-15 20:29:40 +0200

    Improve support for footnotes

--- OfficeIMO.Word/WordFootNote.cs
+++ OfficeIMO.Word/WordFootNote.cs
@@ -19,6 +19,12 @@ namespace OfficeIMO.Word {
             this._run = run;
         }
 
+        /// <summary>
+        /// List of Paragraphs for given FootNote
+        /// As there can be multiple paragraphs with different formatting it's required to provide a list
+        /// Zero based object should be skipped, as it's FootnoteReference
+        /// However for sake of completion and potential ability to modify it we expose it as well
+        /// </summary>
         public List<WordParagraph> Paragraphs {
             get {
                 if (_paragraph != null && _run != null) {
@@ -44,7 +50,24 @@ namespace OfficeIMO.Word {
             }
         }
 
-        public long? ReferenceID {
+        /// <summary>
+        /// Parent Paragraph is Paragraph/Run that has FootNote attached to it.
+        /// This provides ability to find proper Run that has FootNote
+        /// </summary>
+        public WordParagraph ParentParagraph {
+            get {
+                var previousRun = _run.PreviousSibling<Run>();
+                if (previousRun != null) {
+                    return new WordParagraph(_document, _paragraph, previousRun);
+                }
+                return null;
+            }
+        }
+
+        /// <summary>
+        /// ReferenceID of FootNote
+        /// </summary>
+        public long? ReferenceId {
             get {
                 if (_paragraph != null && _run != null) {
                     var footNoteReference = _run.ChildElements.OfType<FootnoteReference>().FirstOrDefault();
@@ -56,57 +79,28 @@ namespace OfficeIMO.Word {
             }
         }
 
-        //public void Remove(bool includingParagraph = false) {
-        //    if (includingParagraph) {
-        //        this._paragraph.Remove();
-        //    } else {
-        //        if (_run.ChildElements.Count == 1) {
-        //            this._run.Remove();
-        //        } else {
-        //            this._run.ChildElements.OfType<TabChar>().FirstOrDefault()?.Remove();
-        //        }
-        //    }
-        //}
-
-        //public List<WordParagraph> Paragraphs {
-        //    get {
-        //        // Get the FootnotesPart
-        //        FootnotesPart footnotesPart = _document._wordprocessingDocument.MainDocumentPart.FootnotesPart;
-
-        //        foreach (Footnote footnote in footnotesPart.Footnotes) {
-        //            // Find the last Paragraph element in the Footnote element.
-        //            Paragraph lastParagraph = footnote.Descendants<Paragraph>().LastOrDefault();
-        //            if (lastParagraph != null) {
-        //                // Get the Text from that Paragraph element
-        //                IEnumerable<Text> paragraphText = lastParagraph.Descendants<Text>();
-        //            }
-        //        }
-
-        //    };
-        //}
-
-        //public Footnote GetFootnoteById(WordprocessingDocument wordprocessingDocument, string id) {
-        //    // Get the FootnotesPart
-        //    FootnotesPart footnotesPart = wordprocessingDocument.MainDocumentPart.FootnotesPart;
-
-        //    // Search for the footnote by ID
-        //    foreach (Footnote footnote in footnotesPart.Footnotes) {
-        //        if (footnote.Id == id) {
-        //            return footnote;
-        //        }
-        //    }
-
-        //    return null;
-        //}
-
-
-
+        /// <summary>
+        /// Remove FootNote from document
+        /// </summary>
+        public void Remove() {
+            long referenceId = 0;
+            var footNoteReference = _run.ChildElements.OfType<FootnoteReference>().FirstOrDefault();
+            if (footNoteReference != null) {
+                referenceId = footNoteReference.Id;
+            }
+            FootnotesPart footnotesPart = _document._wordprocessingDocument.MainDocumentPart.FootnotesPart;
+            var footNotes = footnotesPart.Footnotes.ChildElements.OfType<Footnote>().ToList();
+            foreach (var footNote in footNotes) {
+                if (footNote != null) {
+                    if (footNote.Id == referenceId.ToString()) {
+                        footNote.Remove();
+                    }
+                }
+            }
+            this._run.Remove();
+        }
 
         internal static WordParagraph AddFootNote(WordDocument document, WordParagraph wordParagraph, WordParagraph footerWordParagraph) {
-            // _document = document;
-            // _paragraph = wordParagraph._paragraph;
-
-
             var footerReferenceId = GetNextFooterReferenceId(document);
 
             var newWordParagraph = new WordParagraph(document, wordParagraph._paragraph, true);
@@ -127,19 +121,14 @@ namespace OfficeIMO.Word {
             return newWordParagraph;
         }
 
-
-
         internal static long GetNextFooterReferenceId(WordDocument document) {
             var footNotesPart = document._wordprocessingDocument.MainDocumentPart.FootnotesPart;
             var highestId = footNotesPart.Footnotes.Descendants<Footnote>().Max(fn => fn.Id.Value);
             return (highestId <= 0) ? 1 : highestId + 1;
         }
 
-
         internal static Footnote GenerateFootNote(long footerReferenceId, WordParagraph wordParagraph) {
             Footnote footnote1 = new Footnote() { Id = footerReferenceId };
-
-            //  Paragraph paragraph1 = new Paragraph() { };
 
             ParagraphProperties paragraphProperties1 = new ParagraphProperties();
             ParagraphStyleId paragraphStyleId1 = new ParagraphStyleId() { Val = "FootnoteText" };
@@ -157,47 +146,13 @@ namespace OfficeIMO.Word {
             run1.Append(runProperties1);
             run1.Append(footnoteReferenceMark1);
 
-
             wordParagraph._paragraph.ParagraphProperties = paragraphProperties1;
-            // wordParagraph._run.RunProperties = runProperties1;
 
             var run = wordParagraph._paragraph.GetFirstChild<Run>();
             run.InsertBeforeSelf(run1);
 
-            //var text = wordParagraph._run.GetFirstChild<Text>();
-            //text.InsertBeforeSelf(footnoteReferenceMark1);
-            //wordParagraph._run.Append(footnoteReferenceMark1);
-
-
             footnote1.Append(wordParagraph._paragraph);
-
-            //Run run2 = new Run();
-            //Text text1 = new Text() { Space = SpaceProcessingModeValues.Preserve };
-            //text1.Text = " This is first ";
-
-            //run2.Append(text1);
-            //ProofError proofError1 = new ProofError() { Type = ProofingErrorValues.GrammarStart };
-
-            //Run run3 = new Run();
-            //Text text2 = new Text();
-            //text2.Text = "footnote";
-
-            //run3.Append(text2);
-            //ProofError proofError2 = new ProofError() { Type = ProofingErrorValues.GrammarEnd };
-
-            //paragraph1.Append(paragraphProperties1);
-            //paragraph1.Append(run1);
-            //paragraph1.Append(run2);
-            //paragraph1.Append(proofError1);
-            //paragraph1.Append(run3);
-            //paragraph1.Append(proofError2);
-
-            //footnote1.Append(paragraph1);
             return footnote1;
         }
-
-
-
-
     }
 }
