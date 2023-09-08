3c7e1e0c8df7b444da3eb1fe63e173ad26591229
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2023-09-08 21:37:36 +0200

    Add Protect.AlwaysReadOnly example to demonstrate protection by setting document to "Always Read Only"
    This commit adds the Protect.AlwaysReadOnly example to the codebase. This example demonstrates how to set a document's protection to "Always Read Only" using the OfficeIMO.Word library. It creates a basic document, adds a paragraph with specific formatting, sets the document's AlwaysOpenReadOnly property to true, and saves the document. The commit also includes some console output to display the current and updated values of the AlwaysOpenReadOnly property.

---
+++ OfficeIMO.Examples/Word/Protection/Protect.AlwaysReadOnly.cs
@@ -0,0 +1,26 @@
+using System;
+using DocumentFormat.OpenXml.Wordprocessing;
+using OfficeIMO.Word;
+using Color = SixLabors.ImageSharp.Color;
+
+internal static partial class Protect {
+
+    internal static void Example_ProtectAlwaysReadOnly(string folderPath, bool openWord) {
+        Console.WriteLine("[*] Creating standard document with protection 'Always Read Only'");
+        string filePath = System.IO.Path.Combine(folderPath, "Basic Document with always read only protection.docx");
+        using (WordDocument document = WordDocument.Create(filePath)) {
+            var paragraph = document.AddParagraph("Basic paragraph - Page 4");
+            paragraph.ParagraphAlignment = JustificationValues.Center;
+            paragraph.Color = SixLabors.ImageSharp.Color.Blue;
+            paragraph.AddText(" This is continutation in the same line");
+
+            Console.WriteLine("Always read only: " + document.Settings.AlwaysOpenReadOnly);
+
+            document.Settings.AlwaysOpenReadOnly = true;
+
+            Console.WriteLine("Always read only: " + document.Settings.AlwaysOpenReadOnly);
+
+            document.Save(true);
+        }
+    }
+}
