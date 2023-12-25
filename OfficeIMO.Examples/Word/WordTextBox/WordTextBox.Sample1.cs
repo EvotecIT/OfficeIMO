25838869dfe976a3659c21a3d9b237cf75ffacc0
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2023-12-25 14:18:43 +0100

    Improve example

--- OfficeIMO.Examples/Word/WordTextBox/WordTextBox.Sample1.cs
+++ OfficeIMO.Examples/Word/WordTextBox/WordTextBox.Sample1.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using DocumentFormat.OpenXml.Drawing;
 using OfficeIMO.Word;
+using SixLabors.ImageSharp;
 
 namespace OfficeIMO.Examples.Word {
     internal static partial class WordTextBox {
@@ -14,7 +15,21 @@ namespace OfficeIMO.Examples.Word {
             using (WordDocument document = WordDocument.Create(filePath)) {
                 var paragraph = document.AddParagraph("Adding paragraph with some text");
 
-                document.AddTextBox("[Grab your reader’s attention with a great quote from the document or use this space to emphasize a key point. To place this text box anywhere on the page, just drag it.]");
+                var textBox = document.AddTextBox("[Grab your reader’s attention with a great quote from the document or use this space to emphasize a key point. To place this text box anywhere on the page, just drag it.]");
+
+                Console.WriteLine("TextBox Text: " + textBox.Text);
+
+                textBox.Text = "We can then modify the text box text";
+
+                Console.WriteLine("TextBox Text: " + textBox.WordParagraph.Text);
+
+                Console.WriteLine("TextBoc Color: " + textBox.WordParagraph.Color.ToString());
+
+                textBox.WordParagraph.Text = "This is a text box 1";
+
+                Console.WriteLine("TextBox Text: " + textBox.WordParagraph.Text);
+
+                textBox.WordParagraph.Color = Color.Red;
 
                 document.Save(openWord);
             }
