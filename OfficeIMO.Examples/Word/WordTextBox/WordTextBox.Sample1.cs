daaf5d8db7619ad6f9707a1e3739f05f726c1b21
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2023-12-26 18:07:33 +0100

    Update example

--- OfficeIMO.Examples/Word/WordTextBox/WordTextBox.Sample1.cs
+++ OfficeIMO.Examples/Word/WordTextBox/WordTextBox.Sample1.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using DocumentFormat.OpenXml.Drawing;
+using DocumentFormat.OpenXml.Drawing.Wordprocessing;
 using OfficeIMO.Word;
 using SixLabors.ImageSharp;
 
@@ -9,8 +10,7 @@ namespace OfficeIMO.Examples.Word {
         internal static void Example_AddingTextbox(string folderPath, bool openWord) {
             Console.WriteLine("[*] Creating standard document with some textbox");
 
-            var filePath = System.IO.Path.Combine(folderPath, "BasicDocumentWithTextBox11.docx");
-            var imagePaths = System.IO.Path.Combine(System.IO.Directory.GetCurrentDirectory(), "Images");
+            var filePath = System.IO.Path.Combine(folderPath, "BasicDocumentWithTextBox12.docx");
 
             using (WordDocument document = WordDocument.Create(filePath)) {
                 var paragraph = document.AddParagraph("Adding paragraph with some text");
@@ -30,6 +30,29 @@ namespace OfficeIMO.Examples.Word {
                 Console.WriteLine("TextBox Text: " + textBox.WordParagraph.Text);
 
                 textBox.WordParagraph.Color = Color.Red;
+
+
+                textBox.HorizontalPositionRelativeFrom = HorizontalRelativePositionValues.Page;
+
+                Console.WriteLine("Alignment: " + textBox.HorizontalAlignment);
+
+                textBox.HorizontalAlignment = HorizontalAlignmentValues.Right;
+
+                //                textBox.HorizonalPositionOffset = 1901950;
+
+                //textBox.HorizonalPositionOffsetCentimeters = 1.5;
+
+                Console.WriteLine("Alignment: " + textBox.HorizontalAlignment);
+
+                textBox.VerticalPositionRelativeFrom = VerticalRelativePositionValues.Page;
+
+                textBox.VerticalPositionOffset = 1901950;
+
+                textBox.VerticalPositionOffsetCentimeters = 5;
+
+                Console.WriteLine("Vertical Position Offset: " + textBox.VerticalPositionOffset);
+                Console.WriteLine("Vertical Position Offset in CM: " + textBox.VerticalPositionOffsetCentimeters);
+
 
                 document.Save(openWord);
             }
