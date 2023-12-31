05689322b5063066eb45b4548d04124163d4e045
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2023-12-31 16:00:18 +0100

    Lets check

--- OfficeIMO.Tests/Word.Watermark.cs
+++ OfficeIMO.Tests/Word.Watermark.cs
@@ -24,9 +24,7 @@ namespace OfficeIMO.Tests {
                 Assert.True(watermark.AllowInCell == false);
                 Assert.True(watermark.Rotation == 90);
 
-                Console.WriteLine("Height: " + watermark.Height);
-
-                Assert.True(watermark.Height == 131.95);
+                Assert.True(watermark.Height == 131.95, "Value was " + watermark.Height + " but should be " + "131.95");
                 Assert.True(watermark.Width == 527.85);
                 Assert.True(watermark.ColorHex == "silver");
                 Assert.True(watermark.Color == Color.Silver);
