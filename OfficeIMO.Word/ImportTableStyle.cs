83fda6675c9ed52a556e1f4d69b66b88f3bcf45a
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2024-02-01 17:33:42 +0100

    Changes to get part by id

--- OfficeIMO.Word/ImportTableStyle.cs
+++ OfficeIMO.Word/ImportTableStyle.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
@@ -14,7 +14,7 @@ namespace OfficeIMO.Word {
             using (var repeaterSourceDocument = WordprocessingDocument.Open(sourcefilepath, true)) {
                 XDocument source_style_doc;
 
-                var repeaterSourceDocumentPackagePart = repeaterSourceDocument.MainDocumentPart.OpenXmlPackage.Package.GetPart(new Uri("/word/styles.xml", UriKind.Relative));
+                var repeaterSourceDocumentPackagePart = repeaterSourceDocument.MainDocumentPart.GetPartById("/word/styles.xml");
 
                 //Get styles.xml
                 using (TextReader tr = new StreamReader(repeaterSourceDocumentPackagePart.GetStream())) {
@@ -26,14 +26,14 @@ namespace OfficeIMO.Word {
                 using (var targetFileToImportTableStyles = WordprocessingDocument.Open(destinationfilepath, true)) {
                     XDocument dest_style_doc;
 
-                    var destpart = targetFileToImportTableStyles.MainDocumentPart.OpenXmlPackage.Package.GetPart(new Uri("/word/styles.xml", UriKind.Relative));
+                    var destpart = targetFileToImportTableStyles.MainDocumentPart.GetPartById("/word/styles.xml");
 
                     //Get styles.xml
                     using (TextReader trd = new StreamReader(destpart.GetStream())) {
                         dest_style_doc = XDocument.Load(trd);
                     }
 
-                    //Add all the style elements from source document styles.xml 
+                    //Add all the style elements from source document styles.xml
                     foreach (var styleelement in tableStylesFromRepeaterSource) {
                         if (!dest_style_doc.Elements(XName.Get("styles", w.NamespaceName)).Any(x => (string)x.Attribute("styleId") == (string)styleelement.Attribute("styleId"))) {
                             dest_style_doc.Element(XName.Get("styles", w.NamespaceName)).Add(styleelement);
