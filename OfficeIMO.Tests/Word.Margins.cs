5be68401a1ed35152473b1ea6c38c80e5e63b5dd
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2024-02-04 19:40:50 +0100

    Removes usings

--- OfficeIMO.Tests/Word.Margins.cs
+++ OfficeIMO.Tests/Word.Margins.cs
@@ -1,9 +1,4 @@
-using System;
-using System.Collections.Generic;
 using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using DocumentFormat.OpenXml.Wordprocessing;
 using OfficeIMO.Word;
 using Xunit;
