a796575c90bd40ca99b03ddafc04082976099f7b
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2024-05-12 12:32:17 +0200

    Bring it to master

--- OfficeIMO.VerifyTests/Word/ChartTests.cs
+++ OfficeIMO.VerifyTests/Word/ChartTests.cs
@@ -1,10 +1,15 @@
-﻿using System.Collections.Generic;
+using System.Collections.Generic;
 using System.Threading.Tasks;
+
 using DocumentFormat.OpenXml.Drawing.Charts;
 using DocumentFormat.OpenXml.Packaging;
+
 using OfficeIMO.Word;
+
 using SixLabors.ImageSharp;
+
 using VerifyXunit;
+
 using Xunit;
 
 namespace OfficeIMO.VerifyTests.Word;
@@ -24,44 +29,46 @@ public class ChartTests : VerifyTestBase {
         };
 
         document.AddParagraph("This is a bar chart");
-        var barChart1 = document.AddBarChart();
+        var barChart1 = document.AddChart();
         barChart1.AddCategories(categories);
-        barChart1.AddChartBar("Brazil", new List<int> { 10, 35, 18, 23 }, Color.Brown);
-        barChart1.AddChartBar("Poland", new List<int> { 13, 20, 230, 150 }, Color.Green);
-        barChart1.AddChartBar("USA", new[] { 10, 35, 18, 23 }, Color.AliceBlue);
+        barChart1.AddBar("Brazil", new List<int> { 10, 35, 18, 23 }, Color.Brown);
+        barChart1.AddBar("Poland", new List<int> { 13, 20, 230, 150 }, Color.Green);
+        barChart1.AddBar("USA", new[] { 10, 35, 18, 23 }, Color.AliceBlue);
         barChart1.BarGrouping = BarGroupingValues.Clustered;
         barChart1.BarDirection = BarDirectionValues.Column;
 
         document.AddParagraph("This is a bar chart");
-        var barChart2 = document.AddBarChart();
+        var barChart2 = document.AddChart();
         barChart2.AddCategories(categories);
-        barChart2.AddChartBar("USA", 15, Color.Aqua);
+        barChart2.AddBar("USA", 15, Color.Aqua);
         barChart2.RoundedCorners = true;
 
         document.AddParagraph("This is a pie chart");
         var pieChart = document.AddPieChart();
-        pieChart.AddCategories(categories);
-        pieChart.AddChartPie("Poland", new List<int> { 15, 20, 30 });
+        //pieChart.AddCategories(categories);
+        pieChart.AddPie("Poland", 15);
+        pieChart.AddPie("USA", 25);
+        pieChart.AddPie("Brazil", 60);
 
         document.AddParagraph("Adding a line chart as required 1");
 
-        var lineChart = document.AddLineChart();
+        var lineChart = document.AddChart();
         lineChart.AddChartAxisX(categories);
-        lineChart.AddChartLine("USA", new List<int> { 10, 35, 18, 23 }, Color.AliceBlue);
-        lineChart.AddChartLine("Brazil", new List<int> { 10, 35, 300, 18 }, Color.Brown);
-        lineChart.AddChartLine("Poland", new List<int> { 13, 20, 230, 150 }, Color.Green);
+        lineChart.AddLine("USA", new List<int> { 10, 35, 18, 23 }, Color.AliceBlue);
+        lineChart.AddLine("Brazil", new List<int> { 10, 35, 300, 18 }, Color.Brown);
+        lineChart.AddLine("Poland", new List<int> { 13, 20, 230, 150 }, Color.Green);
 
         document.AddParagraph("Adding a line chart as required 2");
 
-        var lineChart2 = document.AddLineChart();
+        var lineChart2 = document.AddChart();
         lineChart2.AddChartAxisX(categories);
-        lineChart2.AddChartLine("USA", new List<int> { 10, 35, 18, 23 }, Color.AliceBlue);
-        lineChart2.AddChartLine("Brazil", new List<int> { 10, 35, 300, 18 }, Color.Brown);
-        lineChart2.AddChartLine("Poland", new List<int> { 13, 20, 230, 150 }, Color.Green);
+        lineChart2.AddLine("USA", new List<int> { 10, 35, 18, 23 }, Color.AliceBlue);
+        lineChart2.AddLine("Brazil", new List<int> { 10, 35, 300, 18 }, Color.Brown);
+        lineChart2.AddLine("Poland", new List<int> { 13, 20, 230, 150 }, Color.Green);
 
         document.Save();
 
         await DoTest(document._wordprocessingDocument);
     }
-    
+
 }
