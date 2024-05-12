9e059733512eb844753383de281f1b8908125924
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2024-05-12 21:55:31 +0200

    Add documentation builder

---
+++ Build/BuildDocumentation.ps1
@@ -0,0 +1,20 @@
+﻿param (
+    $SolutionRoot = "$PSScriptRoot\.."
+)
+
+$DotNetVersion = 'net8.0'
+$SolutionPath = [io.path]::Combine($SolutionRoot, 'OfficeImo.sln')
+if ($SolutionRoot -and (Test-Path -Path $SolutionPath)) {
+    $DllPath = [io.path]::Combine($SolutionRoot, "OfficeIMO.Word", "bin", "Debug", $DotNetVersion, "OfficeIMO.Word.dll")
+    if (-not (Test-Path -Path $DllPath)) {
+        Write-Host -Object "DLL not found at $DllPath" -ForegroundColor Red
+        return
+    }
+    $DocsPath = [io.path]::Combine($SolutionRoot, "Docs")
+
+    if (-not (Get-Command -Name xmldoc2md -ErrorAction SilentlyContinue)) {
+        Write-Host -Object "xmldoc2md not found" -ForegroundColor Red
+        return
+    }
+    xmldoc2md $DllPath $DocsPath
+}
