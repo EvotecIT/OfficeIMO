00de82bc3dd070e18b303f099d4e6c00564efb3d
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2024-05-12 21:13:53 +0200

    Rename file and improve

---
+++ Build/ApproveVerifyTests.ps1
@@ -0,0 +1,22 @@
+param (
+    $SolutionRoot = "$PSScriptRoot\.."
+)
+
+$ErrorActionPreference = 'Stop'
+$DotnetVersions = @('6.0', '7.0', '8.0')
+$SolutionPath = [io.path]::Combine($SolutionRoot, 'OfficeImo.sln')
+if ($SolutionRoot -and (Test-Path -Path $SolutionPath)) {
+    Write-Host "Solution found at $($SolutionPath). Processing files..." -ForegroundColor Green
+    Get-ChildItem -Recurse $SolutionRoot -Filter "*.received.txt" | ForEach-Object {
+        Write-Host "Approving $($_.FullName)" -ForegroundColor Yellow
+        $ReceivedTestResult = $_.FullName
+        foreach ($DotNetVersion in $DotNetVersions) {
+            if ($ReceivedTestResult -like "*DotNet$DotNetVersion*") {
+                $ApprovedTestResult = $ReceivedTestResult.Replace(".DotNet$DotNetVersion.received.txt", '.verified.txt')
+                Move-Item -LiteralPath $ReceivedTestResult -Destination $ApprovedTestResult -Force
+            }
+        }
+    }
+} else {
+    Write-Host -Object "Solution not found at $SolutionPath" -ForegroundColor Red
+}
