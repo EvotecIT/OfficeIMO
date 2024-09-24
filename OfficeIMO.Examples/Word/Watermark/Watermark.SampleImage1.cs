75bae802c968715d6553e0988e597cd00ab24856
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2024-09-24 22:18:04 +0200

    Remove some code

--- OfficeIMO.Examples/Word/Watermark/Watermark.SampleImage1.cs
+++ OfficeIMO.Examples/Word/Watermark/Watermark.SampleImage1.cs
@@ -1,19 +1,11 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
 using OfficeIMO.Word;
-
-using SixLabors.ImageSharp;
 
 namespace OfficeIMO.Examples.Word {
     internal static partial class Watermark {
         public static void Watermark_SampleImage1(string folderPath, bool openWord) {
             Console.WriteLine("[*] Creating standard document with Watermark Image 1");
             var imagePaths = System.IO.Path.Combine(System.IO.Directory.GetCurrentDirectory(), "Images");
-            string filePath = System.IO.Path.Combine(folderPath, "Basic Document with Watermark Image 1.docx");
+            string filePath = System.IO.Path.Combine(folderPath, "Basic Document with Watermark Image 2.docx");
 
             using (WordDocument document = WordDocument.Create(filePath)) {
                 document.AddParagraph("Section 0");
@@ -22,16 +14,23 @@ namespace OfficeIMO.Examples.Word {
                 var imagePathToAdd = System.IO.Path.Combine(imagePaths, "PrzemyslawKlysAndKulkozaurr.jpg");
                 var watermark = document.Sections[0].Header.Default.AddWatermark(WordWatermarkStyle.Image, imagePathToAdd);
 
-                // TODO: Add handling for watermark image
-                Console.WriteLine(watermark.Height);
-                Console.WriteLine(watermark.Width);
+                //Console.WriteLine(watermark.Height);
+                //Console.WriteLine(watermark.Width);
 
+                //Console.WriteLine("Watermarks in document: " + document.Watermarks.Count);
+                //Console.WriteLine("Images in document: " + document.Images.Count);
+                //Console.WriteLine("Watermarks in header: " + document.Header.Default.Watermarks.Count);
+                //Console.WriteLine("Images in header: " + document.Header.Default.Images.Count);
 
                 document.Save(false);
             }
 
             using (WordDocument document = WordDocument.Load(filePath)) {
 
+                //Console.WriteLine("Watermarks in document: " + document.Watermarks.Count);
+                //Console.WriteLine("Images in document: " + document.Images.Count);
+                //Console.WriteLine("Watermarks in header: " + document.Header.Default.Watermarks.Count);
+                //Console.WriteLine("Images in header: " + document.Header.Default.Images.Count);
                 document.Save(openWord);
             }
         }
