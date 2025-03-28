4e3acd2999a8227535cad891201ae84f1357e2dc
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2025-03-28 23:12:39 +0100

    feat(BorderSide): ✨ add enum for table border sides
    * Introduced `BorderSide` enum to represent various border sides for tables.
    * This addition enhances the customization options for table styling.

---
+++ OfficeIMO.Word/Tables/BorderSide.cs
@@ -0,0 +1,13 @@
+﻿namespace OfficeIMO.Word;
+
+/// <summary>
+/// Enum to represent border sides for tables
+/// </summary>
+public enum BorderSide {
+    Top,
+    Bottom,
+    Left,
+    Right,
+    InsideHorizontal,
+    InsideVertical
+}
