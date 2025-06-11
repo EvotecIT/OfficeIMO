b792a60e58fc82c20816294228e129c27d004c41
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2025-06-11 12:48:02 +0200

    refactor(embed): ✨ Improve code structure for `Example_EmbedFileExcel`
    * Rearranged namespace and class declarations for better readability.
    * Maintained functionality while enhancing code organization.

--- OfficeIMO.Examples/Word/Embed/EmbedObject01.cs
+++ OfficeIMO.Examples/Word/Embed/EmbedObject01.cs
@@ -1,31 +1,23 @@
-using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using DocumentFormat.OpenXml.Packaging;
-using DocumentFormat.OpenXml.Wordprocessing;
 using OfficeIMO.Word;
 
-namespace OfficeIMO.Examples.Word {
-    internal static partial class Embed {
+namespace OfficeIMO.Examples.Word;
 
-        public static void Example_EmbedFileExcel(string folderPath, string templateFolder, bool openWord) {
-            Console.WriteLine("[*] Creating standard document with embedded object (excel file)");
-            string filePath = System.IO.Path.Combine(folderPath, "EmbeddedObjectExcel.docx");
+internal static partial class Embed {
+
+    public static void Example_EmbedFileExcel(string folderPath, string templateFolder, bool openWord) {
+        Console.WriteLine("[*] Creating standard document with embedded object (excel file)");
+        string filePath = System.IO.Path.Combine(folderPath, "EmbeddedObjectExcel.docx");
 
 
-            using (WordDocument document = WordDocument.Create(filePath)) {
-                document.AddParagraph("Add Excel document in front of the document");
+        using (WordDocument document = WordDocument.Create(filePath)) {
+            document.AddParagraph("Add Excel document in front of the document");
 
-                string excelFilePath = System.IO.Path.Combine(templateFolder, "SampleFileExcel.xlsx");
-                var imageFilePath = System.IO.Path.Combine(templateFolder, "SampleExcelIcon.png");
+            string excelFilePath = System.IO.Path.Combine(templateFolder, "SampleFileExcel.xlsx");
+            var imageFilePath = System.IO.Path.Combine(templateFolder, "SampleExcelIcon.png");
 
-                document.AddEmbeddedObject(excelFilePath, imageFilePath);
+            document.AddEmbeddedObject(excelFilePath, imageFilePath);
 
-                document.Save(openWord);
-            }
+            document.Save(openWord);
         }
     }
 }
