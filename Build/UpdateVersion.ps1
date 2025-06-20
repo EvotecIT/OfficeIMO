04a2ef6c1e412e32868f6ab90c3e18a5adf59c16
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2025-06-20 17:30:42 +0200

    feat(update-version): ✨ Add script to update project version
    * Introduced `UpdateVersion.ps1` to automate version updates.
    * Utilizes `Get-ProjectVersion` and `Set-ProjectVersion` for version management.
    * Excludes specified folders during version retrieval.

---
+++ Build/UpdateVersion.ps1
@@ -0,0 +1,6 @@
+﻿Import-Module PSPublishModule -Force
+
+$Path = "$PSScriptRoot\.."
+
+Get-ProjectVersion -Path "$Path" -ExcludeFolders @("$Path\Module\Artefacts") | Format-Table
+Set-ProjectVersion -Path "$Path" -NewVersion "0.0.24" -WhatIf:$false -Verbose -ExcludeFolders @("$Path\Module\Artefacts") | Format-Table
