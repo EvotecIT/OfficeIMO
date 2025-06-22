dc4c5d80f4f263d69ece573b698515e9d1ba9e98
Przemysław Kłys <przemyslaw.klys@evotec.pl>
2025-06-22 11:15:48 +0200

    test: ensure table style duplication is validated

---
+++ OfficeIMO.Tests/Word.TableStyleValidation.cs
@@ -0,0 +1,24 @@
+using System;
+using System.IO;
+using OfficeIMO.Word;
+using Xunit;
+
+namespace OfficeIMO.Tests {
+    public partial class Word {
+        [Fact]
+        public void Test_TableStyles_NoDuplicateValidationErrors() {
+            string filePath = Path.Combine(_directoryWithFiles, "TableStylesValidation.docx");
+            using (WordDocument document = WordDocument.Create(filePath)) {
+                foreach (WordTableStyle style in Enum.GetValues(typeof(WordTableStyle))) {
+                    document.AddTable(1, 1, style);
+                }
+                document.Save();
+            }
+
+            using (WordDocument document = WordDocument.Load(filePath)) {
+                Assert.True(document.DocumentValidationErrors.Count == 0,
+                    Word.FormatValidationErrors(document.DocumentValidationErrors));
+            }
+        }
+    }
+}
