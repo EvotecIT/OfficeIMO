ad6a61f8c41070a039866a5cb3595710bc766a10
Przemysław Kłys <przemyslaw.klys@evotec.pl>
2025-06-24 14:07:13 +0200

    Add XML summaries for WordTabChar

--- OfficeIMO.Word/WordTabChar.cs
+++ OfficeIMO.Word/WordTabChar.cs
@@ -5,17 +5,32 @@ using System.Text;
 using DocumentFormat.OpenXml.Wordprocessing;
 
 namespace OfficeIMO.Word {
+    /// <summary>
+    /// Represents a tab character within a paragraph.
+    /// </summary>
     public class WordTabChar : WordElement {
         private WordDocument _document;
         private readonly Paragraph _paragraph;
         private readonly Run _run;
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="WordTabChar"/> class.
+        /// </summary>
+        /// <param name="document">Parent document.</param>
+        /// <param name="paragraph">Paragraph containing the tab.</param>
+        /// <param name="run">Run that holds the tab character.</param>
         public WordTabChar(WordDocument document, Paragraph paragraph, Run run) {
             this._document = document;
             this._paragraph = paragraph;
             this._run = run;
         }
 
+        /// <summary>
+        /// Removes the tab character from the document. Optionally removes the paragraph.
+        /// </summary>
+        /// <param name="includingParagraph">
+        /// If set to <c>true</c> the paragraph containing the tab is removed.
+        /// </param>
         public void Remove(bool includingParagraph = false) {
             if (includingParagraph) {
                 this._paragraph.Remove();
@@ -28,6 +43,12 @@ namespace OfficeIMO.Word {
             }
         }
 
+        /// <summary>
+        /// Appends a tab character to the specified paragraph.
+        /// </summary>
+        /// <param name="document">Parent document.</param>
+        /// <param name="wordParagraph">Paragraph to add the tab to.</param>
+        /// <returns>A paragraph containing the inserted tab.</returns>
         internal static WordParagraph AddTab(WordDocument document, WordParagraph wordParagraph) {
             var newWordParagraph = new WordParagraph(document, wordParagraph._paragraph, true);
             newWordParagraph._run.Append(new TabChar());
