c4d7eb37d1fc561d1400dcf3987c5ee1f4c9c545
Przemysław Kłys <przemyslaw.klys@evotec.pl>
2025-06-24 23:23:50 +0200

    Add list conversion methods and tests

---
+++ OfficeIMO.Tests/Word.ListConversion.cs
@@ -0,0 +1,55 @@
+using System.IO;
+using System.Linq;
+using DocumentFormat.OpenXml.Wordprocessing;
+using OfficeIMO.Word;
+using Xunit;
+
+namespace OfficeIMO.Tests {
+    public partial class Word {
+        [Fact]
+        public void Test_ConvertBulletToNumbered() {
+            var filePath = Path.Combine(_directoryWithFiles, "ConvertBulletToNumbered.docx");
+            int indent;
+            using (var document = WordDocument.Create(filePath)) {
+                var list = document.AddList(WordListStyle.Bulleted);
+                list.AddItem("One");
+                list.AddItem("Two");
+                indent = list.Numbering.Levels.First().IndentationLeft;
+                list.ConvertToNumbered();
+                Assert.Equal(NumberFormatValues.Decimal, list.Numbering.Levels.First()._level.NumberingFormat.Val.Value);
+                Assert.Equal(indent, list.Numbering.Levels.First().IndentationLeft);
+                document.Save(false);
+            }
+
+            using (var document = WordDocument.Load(filePath)) {
+                var list = document.Lists.First();
+                Assert.Equal(new[] { "One", "Two" }, list.ListItems.Select(i => i.Text).ToArray());
+                Assert.Equal(NumberFormatValues.Decimal, list.Numbering.Levels.First()._level.NumberingFormat.Val.Value);
+                Assert.Equal(indent, list.Numbering.Levels.First().IndentationLeft);
+            }
+        }
+
+        [Fact]
+        public void Test_ConvertNumberedToBullet() {
+            var filePath = Path.Combine(_directoryWithFiles, "ConvertNumberedToBullet.docx");
+            int indent;
+            using (var document = WordDocument.Create(filePath)) {
+                var list = document.AddList(WordListStyle.Headings111);
+                list.AddItem("One");
+                list.AddItem("Two");
+                indent = list.Numbering.Levels.First().IndentationLeft;
+                list.ConvertToBulleted();
+                Assert.Equal(NumberFormatValues.Bullet, list.Numbering.Levels.First()._level.NumberingFormat.Val.Value);
+                Assert.Equal(indent, list.Numbering.Levels.First().IndentationLeft);
+                document.Save(false);
+            }
+
+            using (var document = WordDocument.Load(filePath)) {
+                var list = document.Lists.First();
+                Assert.Equal(new[] { "One", "Two" }, list.ListItems.Select(i => i.Text).ToArray());
+                Assert.Equal(NumberFormatValues.Bullet, list.Numbering.Levels.First()._level.NumberingFormat.Val.Value);
+                Assert.Equal(indent, list.Numbering.Levels.First().IndentationLeft);
+            }
+        }
+    }
+}
