9972b1fb1e29ffffdec80c15431bc9c5ffbea73b
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2025-06-24 18:27:53 +0200

    Add WordTableStyleEnum to define various table styles for Word documents

---
+++ OfficeIMO.Word/WordTableStyle.Helpers copy.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using DocumentFormat.OpenXml;
+using DocumentFormat.OpenXml.Wordprocessing;
+
+namespace OfficeIMO.Word;
+
+public static partial class WordTableStyles {
+
+}
---
+++ OfficeIMO.Word/WordTableStyleEnum.cs
@@ -0,0 +1,138 @@
+﻿namespace OfficeIMO.Word;
+
+public enum WordTableStyle {
+    // Plain Tables
+    TableNormal,
+    TableGrid,
+    PlainTable1,
+    PlainTable2,
+    PlainTable3,
+    PlainTable4,
+    PlainTable5,
+
+    // Grid Tables - Line 1
+    GridTable1Light,
+    GridTable1LightAccent1,
+    GridTable1LightAccent2,
+    GridTable1LightAccent3,
+    GridTable1LightAccent4,
+    GridTable1LightAccent5,
+    GridTable1LightAccent6,
+
+    // Grid Tables - Line 2
+    GridTable2,
+    GridTable2Accent1,
+    GridTable2Accent2,
+    GridTable2Accent3,
+    GridTable2Accent4,
+    GridTable2Accent5,
+    GridTable2Accent6,
+
+    // Grid Tables - Line 3
+    GridTable3,
+    GridTable3Accent1,
+    GridTable3Accent2,
+    GridTable3Accent3,
+    GridTable3Accent4,
+    GridTable3Accent5,
+    GridTable3Accent6,
+
+    // Grid Tables - Line 4
+    GridTable4,
+    GridTable4Accent1,
+    GridTable4Accent2,
+    GridTable4Accent3,
+    GridTable4Accent4,
+    GridTable4Accent5,
+    GridTable4Accent6,
+
+    // Grid Tables - Line 5
+    GridTable5Dark,
+    GridTable5DarkAccent1,
+    GridTable5DarkAccent2,
+    GridTable5DarkAccent3,
+    GridTable5DarkAccent4,
+    GridTable5DarkAccent5,
+    GridTable5DarkAccent6,
+
+    // Grid Tables - Line 6
+    GridTable6Colorful,
+    GridTable6ColorfulAccent1,
+    GridTable6ColorfulAccent2,
+    GridTable6ColorfulAccent3,
+    GridTable6ColorfulAccent4,
+    GridTable6ColorfulAccent5,
+    GridTable6ColorfulAccent6,
+
+    // Grid Tables - Line 7
+    GridTable7Colorful,
+    GridTable7ColorfulAccent1,
+    GridTable7ColorfulAccent2,
+    GridTable7ColorfulAccent3,
+    GridTable7ColorfulAccent4,
+    GridTable7ColorfulAccent5,
+    GridTable7ColorfulAccent6,
+
+    // List Tables - Line 8
+    ListTable1Light,
+    ListTable1LightAccent1,
+    ListTable1LightAccent2,
+    ListTable1LightAccent3,
+    ListTable1LightAccent4,
+    ListTable1LightAccent5,
+    ListTable1LightAccent6,
+
+    // List Tables - Line 9
+    ListTable2,
+    ListTable2Accent1,
+    ListTable2Accent2,
+    ListTable2Accent3,
+    ListTable2Accent4,
+    ListTable2Accent5,
+    ListTable2Accent6,
+
+    // List Tables - Line 10
+    ListTable3,
+    ListTable3Accent1,
+    ListTable3Accent2,
+    ListTable3Accent3,
+    ListTable3Accent4,
+    ListTable3Accent5,
+    ListTable3Accent6,
+
+    // List Tables - Line 11
+    ListTable4,
+    ListTable4Accent1,
+    ListTable4Accent2,
+    ListTable4Accent3,
+    ListTable4Accent4,
+    ListTable4Accent5,
+    ListTable4Accent6,
+
+    // List Tables - Line 12
+    ListTable5Dark,
+    ListTable5DarkAccent1,
+    ListTable5DarkAccent2,
+    ListTable5DarkAccent3,
+    ListTable5DarkAccent4,
+    ListTable5DarkAccent5,
+    ListTable5DarkAccent6,
+
+    // List Tables - Line 13
+    ListTable6Colorful,
+    ListTable6ColorfulAccent1,
+    ListTable6ColorfulAccent2,
+    ListTable6ColorfulAccent3,
+    ListTable6ColorfulAccent4,
+    ListTable6ColorfulAccent5,
+    ListTable6ColorfulAccent6,
+
+    // List Tables - Line 14
+    ListTable7Colorful,
+    ListTable7ColorfulAccent1,
+    ListTable7ColorfulAccent2,
+    ListTable7ColorfulAccent3,
+    ListTable7ColorfulAccent4,
+    ListTable7ColorfulAccent5,
+    ListTable7ColorfulAccent6,
+};
