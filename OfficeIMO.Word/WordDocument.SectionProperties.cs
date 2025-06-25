71b3fae1e79ace7cec65d1197aeb4ee633f1860b
Przemysław Kłys <przemyslaw.klys@evotec.pl>
2025-06-25 12:14:39 +0200

    Add XML docs for section properties

--- OfficeIMO.Word/WordDocument.SectionProperties.cs
+++ OfficeIMO.Word/WordDocument.SectionProperties.cs
@@ -6,6 +6,13 @@ using DocumentFormat.OpenXml.Wordprocessing;
 
 namespace OfficeIMO.Word {
     public partial class WordDocument {
+        /// <summary>
+        /// Gets or sets the orientation of the pages in the first section.
+        /// </summary>
+        /// <remarks>
+        /// When a document contains multiple sections, consider using
+        /// <c>Sections[wantedSection].PageOrientation</c> instead.
+        /// </remarks>
         public PageOrientationValues PageOrientation {
             get {
                 if (this.Sections.Count > 1) {
@@ -23,6 +30,13 @@ namespace OfficeIMO.Word {
             }
         }
 
+        /// <summary>
+        /// Gets or sets the page borders for the first section.
+        /// </summary>
+        /// <remarks>
+        /// When a document contains multiple sections, use
+        /// <c>Sections[wantedSection].Borders</c> to target a specific section.
+        /// </remarks>
         public WordBorders Borders {
             get {
                 if (this.Sections.Count > 1) {
@@ -40,6 +54,13 @@ namespace OfficeIMO.Word {
             }
         }
 
+        /// <summary>
+        /// Gets or sets the margins for the first section.
+        /// </summary>
+        /// <remarks>
+        /// Use <c>Sections[wantedSection].Margins</c> when a document has more
+        /// than one section.
+        /// </remarks>
         public WordMargins Margins {
             get {
                 if (this.Sections.Count > 1) {
@@ -57,6 +78,13 @@ namespace OfficeIMO.Word {
             }
         }
 
+        /// <summary>
+        /// Gets or sets the page size settings for the first section.
+        /// </summary>
+        /// <remarks>
+        /// For multi&#8209;section documents use
+        /// <c>Sections[wantedSection].PageSettings</c> instead.
+        /// </remarks>
         public WordPageSizes PageSettings {
             get {
                 if (this.Sections.Count > 1) {
@@ -74,6 +102,13 @@ namespace OfficeIMO.Word {
             }
         }
 
+        /// <summary>
+        /// Gets or sets the footnote properties for the first section.
+        /// </summary>
+        /// <remarks>
+        /// Use <c>Sections[wantedSection].FootnoteProperties</c> to modify
+        /// other sections.
+        /// </remarks>
         public FootnoteProperties FootnoteProperties {
             get {
                 if (this.Sections.Count > 1) {
@@ -91,6 +126,13 @@ namespace OfficeIMO.Word {
             }
         }
 
+        /// <summary>
+        /// Gets or sets the endnote properties for the first section.
+        /// </summary>
+        /// <remarks>
+        /// Use <c>Sections[wantedSection].EndnoteProperties</c> when working
+        /// with multiple sections.
+        /// </remarks>
         public EndnoteProperties EndnoteProperties {
             get {
                 if (this.Sections.Count > 1) {
@@ -108,6 +150,13 @@ namespace OfficeIMO.Word {
             }
         }
 
+        /// <summary>
+        /// Gets or sets a value indicating whether the gutter should appear on
+        /// the right for right-to-left pages in the first section.
+        /// </summary>
+        /// <remarks>
+        /// For other sections use <c>Sections[wantedSection].RtlGutter</c>.
+        /// </remarks>
         public bool RtlGutter {
             get {
                 if (this.Sections.Count > 1) {
@@ -125,6 +174,13 @@ namespace OfficeIMO.Word {
             }
         }
 
+        /// <summary>
+        /// Adds or updates footnote configuration for the first section.
+        /// </summary>
+        /// <param name="numberingFormat">Numbering format to apply.</param>
+        /// <param name="position">Location of footnotes.</param>
+        /// <param name="restartNumbering">Restart numbering option.</param>
+        /// <param name="startNumber">Starting number.</param>
         public void AddFootnoteProperties(NumberFormatValues? numberingFormat = null,
             FootnotePositionValues? position = null,
             RestartNumberValues? restartNumbering = null,
@@ -132,6 +188,13 @@ namespace OfficeIMO.Word {
             this.Sections[0].AddFootnoteProperties(numberingFormat, position, restartNumbering, startNumber);
         }
 
+        /// <summary>
+        /// Adds or updates endnote configuration for the first section.
+        /// </summary>
+        /// <param name="numberingFormat">Numbering format to apply.</param>
+        /// <param name="position">Location of endnotes.</param>
+        /// <param name="restartNumbering">Restart numbering option.</param>
+        /// <param name="startNumber">Starting number.</param>
         public void AddEndnoteProperties(NumberFormatValues? numberingFormat = null,
             EndnotePositionValues? position = null,
             RestartNumberValues? restartNumbering = null,
@@ -139,6 +202,13 @@ namespace OfficeIMO.Word {
             this.Sections[0].AddEndnoteProperties(numberingFormat, position, restartNumbering, startNumber);
         }
 
+        /// <summary>
+        /// Gets or sets the page numbering configuration for the first section.
+        /// </summary>
+        /// <remarks>
+        /// When a document has multiple sections, access
+        /// <c>Sections[wantedSection].PageNumberType</c> instead.
+        /// </remarks>
         public PageNumberType PageNumberType {
             get {
                 if (this.Sections.Count > 1) {
@@ -156,6 +226,11 @@ namespace OfficeIMO.Word {
             }
         }
 
+        /// <summary>
+        /// Adds or updates page numbering for the first section.
+        /// </summary>
+        /// <param name="startNumber">Starting page number.</param>
+        /// <param name="format">Number format.</param>
         public void AddPageNumbering(int? startNumber = null, NumberFormatValues? format = null) {
             this.Sections[0].AddPageNumbering(startNumber, format);
         }
