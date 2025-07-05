d019a15486232fc63b52797ef6c1dc192105cad0
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2025-07-05 23:09:28 +0200

    fix: 🔧 update project version to 1.0.4
    * Incremented the project version in `UpdateVersion.ps1` from 1.0.3 to 1.0.4.

--- Build/UpdateVersion.ps1
+++ Build/UpdateVersion.ps1
@@ -3,4 +3,4 @@
 $Path = "$PSScriptRoot\.."
 
 Get-ProjectVersion -Path "$Path" -ExcludeFolders @("$Path\Module\Artefacts") | Format-Table
-Set-ProjectVersion -Path "$Path" -NewVersion "1.0.3" -WhatIf:$false -Verbose -ExcludeFolders @("$Path\Module\Artefacts") | Format-Table
+Set-ProjectVersion -Path "$Path" -NewVersion "1.0.4" -WhatIf:$false -Verbose -ExcludeFolders @("$Path\Module\Artefacts") | Format-Table
