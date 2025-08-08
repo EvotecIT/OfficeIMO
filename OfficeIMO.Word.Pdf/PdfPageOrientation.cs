3e1a5a9ea843a5a735fc9e839e165ed18d4f49b1
Przemysław Kłys <przemyslaw.klys@evotec.pl>
2025-08-08 12:06:14 +0200

    Add PDF orientation member documentation

--- OfficeIMO.Word.Pdf/PdfPageOrientation.cs
+++ OfficeIMO.Word.Pdf/PdfPageOrientation.cs
@@ -3,7 +3,14 @@ namespace OfficeIMO.Word.Pdf {
     /// Specifies page orientation for PDF export.
     /// </summary>
     public enum PdfPageOrientation {
+        /// <summary>
+        /// Portrait orientation.
+        /// </summary>
         Portrait,
+
+        /// <summary>
+        /// Landscape orientation.
+        /// </summary>
         Landscape
     }
 }
