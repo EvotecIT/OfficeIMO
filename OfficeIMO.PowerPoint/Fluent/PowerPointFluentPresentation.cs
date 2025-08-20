8ff949a8104a235c6205b05e522f747ee297a582
Przemysław Kłys <przemyslaw.klys@evotec.pl>
2025-08-20 14:16:31 +0200

    Add fluent End chaining for PowerPoint

--- OfficeIMO.PowerPoint/Fluent/PowerPointFluentPresentation.cs
+++ OfficeIMO.PowerPoint/Fluent/PowerPointFluentPresentation.cs
@@ -16,22 +16,26 @@ namespace OfficeIMO.PowerPoint.Fluent {
         internal PowerPointPresentation Presentation { get; }
 
         /// <summary>
-        ///     Adds a new slide to the presentation.
+        ///     Adds and optionally configures a new slide to the presentation.
         /// </summary>
         /// <param name="masterIndex">Index of the slide master.</param>
         /// <param name="layoutIndex">Index of the slide layout.</param>
-        public PowerPointSlideBuilder Slide(int masterIndex = 0, int layoutIndex = 0) {
+        /// <param name="configure">Optional action used to configure the slide via a <see cref="PowerPointSlideBuilder"/>.</param>
+        public PowerPointFluentPresentation Slide(int masterIndex = 0, int layoutIndex = 0, Action<PowerPointSlideBuilder> configure = null) {
             PowerPointSlide slide = Presentation.AddSlide(masterIndex, layoutIndex);
-            return new PowerPointSlideBuilder(slide);
+            if (configure != null) {
+                PowerPointSlideBuilder builder = new PowerPointSlideBuilder(slide);
+                configure(builder);
+            }
+            return this;
         }
 
         /// <summary>
         ///     Adds and configures a new slide using a builder action.
         /// </summary>
+        /// <param name="configure">Action used to configure the slide.</param>
         public PowerPointFluentPresentation Slide(Action<PowerPointSlideBuilder> configure) {
-            PowerPointSlideBuilder builder = Slide();
-            configure(builder);
-            return this;
+            return Slide(0, 0, configure);
         }
 
         public PowerPointPresentation End() {
