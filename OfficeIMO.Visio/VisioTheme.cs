8922e371efa540fffbf8ae69280ef091f66ce302
Przemysław Kłys <przemyslaw.klys@evotec.pl>
2025-08-26 19:43:30 +0200

    Fix initial warnings in Visio project

--- OfficeIMO.Visio/VisioTheme.cs
+++ OfficeIMO.Visio/VisioTheme.cs
@@ -3,6 +3,9 @@ namespace OfficeIMO.Visio {
     /// Represents a Visio theme.
     /// </summary>
     public class VisioTheme {
+        /// <summary>
+        /// Name of the theme.
+        /// </summary>
         public string? Name { get; set; }
     }
 }
