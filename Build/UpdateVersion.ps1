ee996d66d4605db01450152d3da7f82d006ed3ff
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2025-08-27 19:20:18 +0200

    fix(version): 🔧 update version prefix to 1.0.7 across multiple projects
    * Updated `VersionPrefix` from `1.0.6` to `1.0.7` in:
      - `OfficeIMO.Word`
      - `OfficeIMO.Word.Html`
      - `OfficeIMO.Word.Markdown`
      - `OfficeIMO.Word.Pdf`
      - `OfficeIMO.Excel`
      - `OfficeIMO.PowerPoint`
      - `OfficeIMO.Visio`
      - `OfficeIMO.Examples`
    * Ensures consistency in versioning across all modules.

--- Build/UpdateVersion.ps1
+++ Build/UpdateVersion.ps1
@@ -3,4 +3,4 @@
 $Path = "$PSScriptRoot\.."
 
 Get-ProjectVersion -Path "$Path" -ExcludeFolders @("$Path\Module\Artefacts") | Format-Table
-Set-ProjectVersion -Path "$Path" -NewVersion "1.0.6" -WhatIf:$false -Verbose -ExcludeFolders @("$Path\Module\Artefacts") | Format-Table
+Set-ProjectVersion -Path "$Path" -NewVersion "1.0.7" -WhatIf:$false -Verbose -ExcludeFolders @("$Path\Module\Artefacts") | Format-Table
