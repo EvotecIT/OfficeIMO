b948ec533cc6a9af2fa170a7021f5cedfb332047
Przemysław Kłys <przemyslaw.klys@evotec.pl>
2025-09-10 13:03:21 +0200

    feat(versioning): ✨ Update version to `0.2.0` in project files
    * Updated `VersionPrefix` in `OfficeIMO.Markdown.csproj` to `0.2.0`.
    * Changed `Set-ProjectVersion` in `UpdateVersion.ps1` to reflect the new version.
    * Ensured compatibility with additional target frameworks.

--- Build/Markdown/UpdateVersion.ps1
+++ Build/Markdown/UpdateVersion.ps1
@@ -3,4 +3,4 @@
 $Path = "$PSScriptRoot\..\..\OfficeIMO.Markdown"
 
 Get-ProjectVersion -Path "$Path" -ExcludeFolders @("$Path\Module\Artefacts") | Format-Table
-Set-ProjectVersion -Path "$Path" -NewVersion "0.1" -WhatIf:$true -Verbose -ExcludeFolders @("$Path\Module\Artefacts") | Format-Table
+Set-ProjectVersion -Path "$Path" -NewVersion "0.2.0" -WhatIf:$false -Verbose -ExcludeFolders @("$Path\Module\Artefacts") | Format-Table
