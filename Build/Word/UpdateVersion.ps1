a799a7dd9e15af5cf5bf78663d90db6bc24d9e09
Przemysław Kłys <przemyslaw.klys@evotec.pl>
2025-11-27 14:37:21 +0100

    chore: 🔧 update version prefixes for Excel, Markdown, and Word modules
    * Updated `VersionPrefix` in `OfficeIMO.Excel.csproj` to `0.6.0`
    * Updated `VersionPrefix` in `OfficeIMO.Markdown.csproj` to `0.5.0`
    * Updated `VersionPrefix` in `OfficeIMO.Word.csproj` to `1.0.20`
    * Adjusted version numbers to reflect new releases

--- Build/Word/UpdateVersion.ps1
+++ Build/Word/UpdateVersion.ps1
@@ -3,4 +3,4 @@
 $Path = "$PSScriptRoot\..\..\OfficeIMO.Word"
 
 Get-ProjectVersion -Path "$Path" -ExcludeFolders @("$Path\Module\Artefacts") | Format-Table
-Set-ProjectVersion -Path "$Path" -NewVersion "1.0.10" -WhatIf:$false -Verbose -ExcludeFolders @("$Path\Module\Artefacts") | Format-Table
+Set-ProjectVersion -Path "$Path" -NewVersion "1.0.20" -WhatIf:$false -Verbose -ExcludeFolders @("$Path\Module\Artefacts") | Format-Table
