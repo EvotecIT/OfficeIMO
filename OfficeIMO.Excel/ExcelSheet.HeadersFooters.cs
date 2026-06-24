c4f3b9c4c0167ab4e62ae11766b8cf278e4fbd0f
Przemysław Kłys <przemyslaw.klys@evotec.pl>
2026-06-24 22:05:30 +0200

    Add header footer image visual baseline

--- OfficeIMO.Excel/ExcelSheet.HeadersFooters.cs
+++ OfficeIMO.Excel/ExcelSheet.HeadersFooters.cs
@@ -707,39 +707,16 @@ namespace OfficeIMO.Excel {
                 } catch { wPt = wPt <= 0 ? 144.0 : wPt; hPt = hPt <= 0 ? 48.0 : hPt; }
             }
 
-            // 4) Write minimal VML markup with a shape for the selected section
+            // 4) Upsert the VML shape for the selected section while preserving other header/footer images.
             string shapeId = isHeader
                 ? (position == HeaderFooterPosition.Left ? "LH" : position == HeaderFooterPosition.Center ? "CH" : "RH")
                 : (position == HeaderFooterPosition.Left ? "LF" : position == HeaderFooterPosition.Center ? "CF" : "RF");
 
-            string vml = $@"<?xml version=""1.0"" encoding=""UTF-8""?>
-<xml xmlns:v=""urn:schemas-microsoft-com:vml"" xmlns:o=""urn:schemas-microsoft-com:office:office"" xmlns:x=""urn:schemas-microsoft-com:office:excel"" xmlns:r=""http://schemas.openxmlformats.org/officeDocument/2006/relationships"">
-  <o:shapelayout v:ext=""edit""><o:idmap v:ext=""edit"" data=""1""/></o:shapelayout>
-  <v:shapetype id=""_x0000_t75"" coordsize=""21600,21600"" o:spt=""75"" o:preferrelative=""t"" path=""m@4@5l@4@11@9@11@9@5xe"" filled=""f"" stroked=""f"">
-    <v:stroke joinstyle=""miter""/>
-    <v:formulas>
-      <v:f eqn=""if lineDrawn pixelLineWidth 0""/>
-      <v:f eqn=""sum @0 1 0""/>
-      <v:f eqn=""sum 0 0 @1""/>
-      <v:f eqn=""prod @2 1 2""/>
-      <v:f eqn=""prod @3 21600 pixelWidth""/>
-      <v:f eqn=""prod @3 21600 pixelHeight""/>
-      <v:f eqn=""sum @0 0 1""/>
-      <v:f eqn=""prod @6 1 2""/>
-      <v:f eqn=""prod @7 21600 pixelWidth""/>
-      <v:f eqn=""sum @8 21600 0""/>
-      <v:f eqn=""prod @7 21600 pixelHeight""/>
-      <v:f eqn=""sum @10 21600 0""/>
-    </v:formulas>
-    <v:path o:extrusionok=""f"" gradientshapeok=""t"" o:connecttype=""rect""/>
-    <o:lock v:ext=""edit"" aspectratio=""t""/>
-  </v:shapetype>
-  <v:shape id=""{shapeId}"" o:spid=""_x0000_s1025"" type=""#_x0000_t75"" style=""position:absolute;margin-left:0;margin-top:0;width:{wPt.ToString(CultureInfo.InvariantCulture)}pt;height:{hPt.ToString(CultureInfo.InvariantCulture)}pt;z-index:1"">
-    <v:imagedata r:id=""{imgRelId}"" o:relid=""{imgRelId}"" o:title=""""/>
-    </v:shape>
-</xml>";
-
-            using (var ms = new MemoryStream(Encoding.UTF8.GetBytes(vml))) {
+            XDocument vmlDocument = LoadOrCreateHeaderFooterVmlDocument(vmlPart);
+            UpsertHeaderFooterVmlShape(vmlDocument, shapeId, imgRelId, wPt, hPt);
+            using (var ms = new MemoryStream()) {
+                vmlDocument.Save(ms, SaveOptions.DisableFormatting);
+                ms.Position = 0;
                 vmlPart.FeedData(ms);
             }
 
